89d45c3f65ce4acfeba0f7ae42ba40bef438f9b1
Randal T Meyer <randal.t.meyer@gmail.com>
2022-02-18 23:53:00 -0800

    better dev script

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+BUILD=
+FULL=
+while test $# -gt 0; do
+	case "$1" in
+		-b) BUILD=true; shift; ;;
+		-f) FULL=true; shift; ;;
+		*) break; ;;
+	esac
+done
+
+
 #region consts and imports
 
 # bring in all the config information
@@ -12,25 +23,31 @@ fi
 #endregion
 
 echoAndDo "cd $sageRootDir"
-echoAndDo "/bin/bash ./scripts/pre-build.sh"
-echoAndDo "tsc --build tsconfig.json"
-echoAndDo "/bin/bash ./scripts/post-build.sh"
-echoAndDo "/bin/bash ./scripts/run-tests.sh"
-
-sageDataDir="$sageRootDir/dist/data/sage"
-
-if [ ! -d "$sageDataDir/bots" ]; then
-	echo "$sageDataDir/bots folder not found!"
-	exit 1
+if [ "$FULL" = "true" ]; then
+	echoAndDo "/bin/bash ./scripts/pre-build.sh"
 fi
+if [ "$FULL" = "true" ] || [ "$BUILD" = "true" ]; then
+	echoAndDo "tsc --build tsconfig.json"
+fi
+if [ "$FULL" = "true" ]; then
+	echoAndDo "/bin/bash ./scripts/post-build.sh"
+	echoAndDo "/bin/bash ./scripts/run-tests.sh"
 
-# make sure we have the sage data sub folders
-sageDataSubs=( "backup" "dice" "games" "logs" "logs/dev" "logs/beta" "logs/stable" "messages" "servers" "users" )
-for sageDataSub in "${sageDataSubs[@]}"; do
-	if [ ! -d $sageDataDir/$sageDataSub ]; then
-		echoAndDo "mkdir $sageDataDir/$sageDataSub"
+	sageDataDir="$sageRootDir/dist/data/sage"
+
+	if [ ! -d "$sageDataDir/bots" ]; then
+		echo "$sageDataDir/bots folder not found!"
+		exit 1
 	fi
-done
+
+	# make sure we have the sage data sub folders
+	sageDataSubs=( "backup" "dice" "games" "logs" "logs/dev" "logs/beta" "logs/stable" "messages" "servers" "users" )
+	for sageDataSub in "${sageDataSubs[@]}"; do
+		if [ ! -d $sageDataDir/$sageDataSub ]; then
+			echoAndDo "mkdir $sageDataDir/$sageDataSub"
+		fi
+	done
+fi
 
 # ensure we have schema.json files
 # if [ ! -f $sageBotDir/sage/schema.json ]; then echoAndDo "cd $sageBotDir; /bin/bash schema.sh"; fi
