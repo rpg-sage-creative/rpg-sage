007bb24fab0133b7c4e75faee52a47e435c1f93a
Randal T Meyer <randal.t.meyer@gmail.com>
2022-02-23 12:17:04 -0800

    code cleanup/reuse

--- src/sage-lib/sage/repo/DialogMessageRepository.ts
+++ src/sage-lib/sage/repo/DialogMessageRepository.ts
@@ -3,13 +3,18 @@ import type { DiscordKey } from "../../discord";
 import type { TDialogMessage } from "../model/GameCharacter";
 import IdRepository from "./base/IdRepository";
 
+function toPath(discordKey: DiscordKey): string {
+	return `${IdRepository.DataPath}/messages/${discordKey.shortKey}.json`;
+}
+
 export default class DialogMessageRepository {
 	public static async read(discordKey: DiscordKey): Promise<TDialogMessage | null> {
-		return utils.FsUtils.readJsonFile<TDialogMessage>(`${IdRepository.DataPath}/messages/${discordKey.shortKey}.json`)
+		const path = toPath(discordKey);
+		return utils.FsUtils.readJsonFile<TDialogMessage>(path)
 			.catch(utils.ConsoleUtils.Catchers.errorReturnNull);
 	}
 	public static async write(discordKey: DiscordKey, dialogMessage: TDialogMessage): Promise<boolean> {
-		const path = `${IdRepository.DataPath}/messages/${discordKey.shortKey}.json`;
+		const path = toPath(discordKey);
 		return utils.FsUtils.writeFile(path, dialogMessage)
 			.catch(utils.ConsoleUtils.Catchers.errorReturnFalse);
 	}
