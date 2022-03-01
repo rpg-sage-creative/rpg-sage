7d44d090dba627c1136761172b5e9b50a95f53d2
Randal T Meyer <randal.t.meyer@gmail.com>
2022-02-28 16:59:47 -0800

    fix content type of post

--- src/sage-utils/utils/HttpsUtils/index.ts
+++ src/sage-utils/utils/HttpsUtils/index.ts
@@ -12,13 +12,7 @@ export function getText(url: string): Promise<string>;
  * Sending postData will JSON.stringify the value and then do a POST instead of a GET.
 */
 export function getText<T = any>(url: string, postData: T): Promise<string>;
-/**
- * You can pass in a fully formed url or leave off the protocol and allow it to prepend "https://".
- * If you pass in a url with "http://" it will downgrade to use http protocol instead of https.
- * Sending postData will JSON.stringify the value and then do a POST instead of a GET.
-*/
-export function getText<T = any>(url: string, postData: T, stringifier: (postData: T) => string): Promise<string>;
-export function getText<T = any>(url: string, postData?: T, stringifier?: (postData: T) => string): Promise<string> {
+export function getText<T = any>(url: string, postData?: T): Promise<string> {
 	if (typeof(url) !== "string") {
 		return Promise.reject("Invalid Url");
 	}
@@ -27,9 +21,16 @@ export function getText<T = any>(url: string, postData?: T, stringifier?: (postD
 	}
 	const protocol = url.match(/^http:\/\//i) ? http : https;
 	const method = postData ? "request" : "get";
+	const payload = postData ? JSON.stringify(postData) : null;
 	return new Promise((resolve, reject) => {
 		try {
-			const req = protocol[method](url, response => {
+			const options = payload ? {
+				headers: {
+					'Content-Type': 'application/json',
+					'Content-Length': payload.length,
+				}
+			} : { };
+			const req = protocol[method](url, options, response => {
 				try {
 					const chunks: string[] = [];
 					response.on("data", chunk =>
@@ -48,8 +49,7 @@ export function getText<T = any>(url: string, postData?: T, stringifier?: (postD
 			req.once("error", reject);
 			req.once("timeout", reject);
 			if (method === "request") {
-				const stringified = stringifier ? stringifier(postData!) : JSON.stringify(postData);
-				req.write(stringified);
+				req.write(payload);
 				req.end();
 			}
 		}catch(ex) {
@@ -61,10 +61,9 @@ export function getText<T = any>(url: string, postData?: T, stringifier?: (postD
 /** Convenience wrapper for getText(url).then(text => JSON.parse(text)) */
 export function getJson<T = any>(url: string): Promise<T>;
 export function getJson<T = any, U = any>(url: string, postData: U): Promise<T>;
-export function getJson<T = any, U = any>(url: string, postData: U, stringifier: (postData: U) => string): Promise<T>;
-export function getJson<T = any, U = any>(url: string, postData?: U, stringifier?: (postData: U) => string): Promise<T> {
+export function getJson<T = any, U = any>(url: string, postData?: U): Promise<T> {
 	return new Promise((resolve, reject) => {
-		getText(url, postData!, stringifier!).then(text => {
+		getText(url, postData).then(text => {
 			try {
 				resolve(JSON.parse(text));
 			}catch(ex) {
