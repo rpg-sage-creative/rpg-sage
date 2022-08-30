a981862df6646d1c9d56804315a9a644cbe3125e
Randal T Meyer <randal.t.meyer@gmail.com>
2022-08-29 22:12:10 -0700

    logging tweaks nilsnowflake default member

--- src/sage-lib/discord/DiscordCache.ts
+++ src/sage-lib/discord/DiscordCache.ts
@@ -15,20 +15,31 @@ function isDiscordApiErrorMissingPermissionsFetchWebhook(reason: any): boolean {
 	return stringValue.includes("DiscordAPIError: Missing Permissions")
 		&& stringValue.includes("TextChannel.fetchWebhooks");
 }
-function isDiscordApiError(reason: any): boolean {
+type TDiscordApiError = {
+	name: "DiscordAPIError";
+	message: string;
+	path: string;
+	/** https://discord.com/developers/docs/topics/opcodes-and-status-codes#json-json-error-codes */
+	code: number;
+	method: "GET"|"POST"|"PATCH"|"PUT"|"DELETE"
+};
+function isDiscordApiError(reason: any): reason is TDiscordApiError {
 	return reason?.name === "DiscordAPIError";
 }
-function isUnknownGuild(reason: any): boolean {
+function isUnknownGuild(reason: TDiscordApiError): boolean {
 	return reason?.message === "Unknown Guild";
 }
-function isUnknownMember(reason: any): boolean {
+function isUnknownMember(reason: TDiscordApiError): boolean {
 	return reason?.message === "Unknown Member";
+}
+function isUnknownUser(reason: TDiscordApiError): boolean {
+	return reason?.message === "Unknown User";
 }
 function warnUnknownElseErrorReturnNull(reason: any): null {
 	if (isDiscordApiErrorMissingPermissionsFetchWebhook(reason)) {
 		console.warn(`DiscordAPIError: Missing Permissions (TextChannel.fetchWebhooks)`);
 	}else {
-		if (isDiscordApiError(reason) && (isUnknownMember(reason) || isUnknownGuild(reason))) {
+		if (isDiscordApiError(reason) && (isUnknownMember(reason) || isUnknownGuild(reason) || isUnknownUser(reason))) {
 			console.warn(`${reason.message}: ${reason.path}`);
 		}else {
 			console.error(reason);
@@ -139,7 +150,7 @@ export default class DiscordCache {
 		return "ERROR_FETCHING_GUILD_CHANNEL";
 	}
 
-	private guildMemberMap = new Map<Snowflake, GuildMember | null>();
+	private guildMemberMap = new Map<Snowflake, GuildMember | null>([[NilSnowflake, null]]);
 	public async fetchGuildMember(userDid: Snowflake): Promise<GuildMember | null> {
 		if (!this.guildMemberMap.has(userDid)) {
 			const guildMember = await this.guild?.members.fetch({ user:userDid, cache:true, force:true }).catch(warnUnknownElseErrorReturnNull);
