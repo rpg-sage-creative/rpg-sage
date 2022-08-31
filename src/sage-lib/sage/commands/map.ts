c134860532fe1de7665246175efd5168411b22f6
Randal T Meyer <randal.t.meyer@gmail.com>
2022-08-31 10:56:02 -0700

    turn off map create slash command

--- src/sage-lib/sage/commands/map.ts
+++ src/sage-lib/sage/commands/map.ts
@@ -567,18 +567,18 @@ function mapCommand(): TSlashCommand {
 		name: "Map",
 		description: "Map Commands",
 		children: [
-			{
-				name: "Create",
-				description: "Sets the map for this channel.",
-				options: [
-					{ name:"url", description:"Url to the map image.", isRequired },
-					{ name:"name", description:"What do you call this map?", isRequired },
-					{ name:"cols", description:"How many columns on this map?", isNumber, isRequired },
-					{ name:"rows", description:"How many rows on this map?", isNumber, isRequired },
-					{ name:"spawn", description:"Grid location to spawn new images. Default: 1,1 " },
-					{ name:"clip", description:"(Advanced) How to clip map's source image: dx,dY,dW,dH" }
-				]
-			},
+			// {
+			// 	name: "Create",
+			// 	description: "Sets the map for this channel.",
+			// 	options: [
+			// 		{ name:"url", description:"Url to the map image.", isRequired },
+			// 		{ name:"name", description:"What do you call this map?", isRequired },
+			// 		{ name:"cols", description:"How many columns on this map?", isNumber, isRequired },
+			// 		{ name:"rows", description:"How many rows on this map?", isNumber, isRequired },
+			// 		{ name:"spawn", description:"Grid location to spawn new images. Default: 1,1 " },
+			// 		{ name:"clip", description:"(Advanced) How to clip map's source image: dx,dY,dW,dH" }
+			// 	]
+			// },
 			{
 				name: "AddImage",
 				description: "Adds an image to a map",
