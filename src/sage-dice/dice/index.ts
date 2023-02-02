08cb942c1822668474df852f087bd58afc0d4629
Randal T Meyer <randal.t.meyer@gmail.com>
2023-02-02 12:53:06 -0800

    shareable emoji correction function

--- src/sage-dice/dice/index.ts
+++ src/sage-dice/dice/index.ts
@@ -1,4 +1,13 @@
+import { createDiscordEmojiRegex } from "../../sage-utils/utils/StringUtils";
+
 export * as base from "./base";
 export * as discord from "./discord";
 export * as pf2e from "./pf2e";
 export * as quest from "./quest";
+
+/** Looks for text escaped with ` characters that contain emoji (:die: or <:die:12345>) and unescapes those emoji so they render correctly. */
+export function correctEscapeForEmoji(value: string): string {
+	return value.replace(/`[^`]+`/g, escapedValue =>
+		escapedValue.replace(createDiscordEmojiRegex(true), emoji => `\`${emoji}\``)
+	);
+}
