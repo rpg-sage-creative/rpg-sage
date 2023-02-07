14331c3a7e00bddaef43b0ac80c13e56331ac09d
Randal T Meyer <randal.t.meyer@gmail.com>
2023-02-07 10:58:12 -0800

    updated httputils with follow-redirects

--- src/sage-utils/utils/HttpsUtils/index.ts
+++ src/sage-utils/utils/HttpsUtils/index.ts
@@ -1,7 +1,19 @@
-import * as http from "http";
-import * as https from "https";
+import type * as coreHttp from "http";
+import type * as coreHttps from "https";
+import * as _followRedirects from "follow-redirects";
+import type { Override, RedirectScheme } from "follow-redirects";
+const { http, https } = (_followRedirects as any).default as typeof _followRedirects;
 
-export function getProtocol(url: string): typeof http | typeof https {
+export type thttp = Override<
+    typeof coreHttp,
+    RedirectScheme<coreHttp.RequestOptions, coreHttp.ClientRequest, coreHttp.IncomingMessage>
+>;
+export type thttps = Override<
+    typeof coreHttps,
+    RedirectScheme<coreHttps.RequestOptions, coreHttp.ClientRequest, coreHttp.IncomingMessage>
+>;
+
+export function getProtocol(url: string): thttp | thttps {
 	return url.match(/^http:\/\//i) ? http : https;
 }
 
