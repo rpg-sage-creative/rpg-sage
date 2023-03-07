09a3958f5e2920364f504c678759c21bffb9acec
Randal T Meyer <randal.t.meyer@gmail.com>
2023-03-07 15:42:13 -0800

    spaces to tabs

--- src/sage-utils/utils/ClassUtils/SuperClass.ts
+++ src/sage-utils/utils/ClassUtils/SuperClass.ts
@@ -5,42 +5,42 @@ import ClassCache from "./ClassCache";
  */
 export default abstract class SuperClass {
 
-    /** Provides a caching mechanism for all SuperClass classes. */
-    private _cache: ClassCache | undefined;
-    protected get cache(): ClassCache {
-        return this._cache ?? (this._cache = new ClassCache());
-    }
+	/** Provides a caching mechanism for all SuperClass classes. */
+	private _cache: ClassCache | undefined;
+	protected get cache(): ClassCache {
+		return this._cache ?? (this._cache = new ClassCache());
+	}
 
-    /** Causes all HasCore descendents to have a default toString() result of '[object ClassName]' */
+	/** Causes all HasCore descendents to have a default toString() result of '[object ClassName]' */
 	public get [Symbol.toStringTag]() {
-        return (this.constructor as typeof SuperClass).toStringTag;
-    }
+		return (this.constructor as typeof SuperClass).toStringTag;
+	}
 
-    /** This allows children to override the toStringTag without needing to override the instance get and the static instanceOf */
-    protected static get toStringTag(): string {
-        return this.name;
-    }
+	/** This allows children to override the toStringTag without needing to override the instance get and the static instanceOf */
+	protected static get toStringTag(): string {
+		return this.name;
+	}
 
 	/** This implementation of instanceof allows for the chance that the same class may be loaded twice in different contexts. */
 	public static instanceOf<T extends SuperClass>(value: any): value is T;
-    public static instanceOf<T extends typeof SuperClass>(value: any, klass: T): value is InstanceType<T>;
-    public static instanceOf<T extends typeof SuperClass>(value: any, klass?: T): value is InstanceType<T> {
-        // attempt a proper instanceof
-        klass = klass ?? this as T;
-        if (value instanceof klass) {
-            return true;
-        }
+	public static instanceOf<T extends typeof SuperClass>(value: any, klass: T): value is InstanceType<T>;
+	public static instanceOf<T extends typeof SuperClass>(value: any, klass?: T): value is InstanceType<T> {
+		// attempt a proper instanceof
+		klass = klass ?? this as T;
+		if (value instanceof klass) {
+			return true;
+		}
 
-        // walk the chain just in case
-        if (value && "instanceOf" in klass) {
-            let proto = value.__proto__;
-            do {
-                if (klass.toStringTag === proto[Symbol.toStringTag]) {
-                    return true;
-                }
-            }while (proto = proto.__proto__);
-        }
-        return false;
-    }
+		// walk the chain just in case
+		if (value && "instanceOf" in klass) {
+			let proto = value.__proto__;
+			do {
+				if (klass.toStringTag === proto[Symbol.toStringTag]) {
+					return true;
+				}
+			}while (proto = proto.__proto__);
+		}
+		return false;
+	}
 
 }
