91fdf7617ad5c01d1802f37426b7e48e80d263c9
Randal T Meyer <randal.t.meyer@gmail.com>
2023-03-31 16:45:01 -0700

    sage-utils cleaned up

--- src/sage-utils/utils/consts.ts
+++
@@ -1,5 +0,0 @@
-export * from "./ColorUtils/consts";
-export * from "./DateUtils/consts";
-export * from "./JsonUtils/consts";
-export * from "./StringUtils/consts";
-export * from "./UuidUtils/consts";
--- src/sage-utils/utils/enums.ts
+++
@@ -1,3 +0,0 @@
-export * from "./ConsoleUtils/enums";
-export * from "./DateUtils/enums";
-export * from "./RenderUtils/enums";
