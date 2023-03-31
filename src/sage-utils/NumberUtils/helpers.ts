86d226a0f4e4bb5cb8aa3a1bdf49bac2e45eab8d
Randal T Meyer <randal.t.meyer@gmail.com>
2023-03-31 15:36:15 -0700

    NumberUtils cleaned up

--- src/sage-utils/NumberUtils/helpers.ts
+++ src/sage-utils/NumberUtils/helpers.ts
@@ -1,12 +1,12 @@
-/** Adds commas to the given. Ex: 12345 becomes "12,345" */
+/** Adds commas to the given value. Ex: 12345 becomes "12,345" */
 export function addCommas(value: number): string {
 	if (value < 1000) {
 		return String(value);
 	}
-	const parts: string[] = String(value).split(/\./),
-		left: string = parts[0],
-		right: string = parts[1] || "",
-		array: string[] = Array.from(left) ?? [],
+	const parts = String(value).split(/\./),
+		left = parts[0],
+		right = parts[1] ?? "",
+		array = Array.from(left) ?? [],
 		output: string[] = [];
 	let i = 1;
 	while (array.length) {
@@ -23,7 +23,7 @@ export function addCommas(value: number): string {
 }
 
 /** Adds the appropriate suffix to the given number. Ex: 1 becomes "1st" */
-export function nth(number: number) {
+export function nth(number: number): string {
 	const digit = +String(number).slice(-1);
 	if (number !== 11 && digit === 1) {
 		return `${number}st`;
@@ -53,8 +53,8 @@ export function round(value: number, decimals: number): number {
 // }
 */
 
-const SUPERSCRIPT_NUMBERS = ["⁰", "¹", "²", "³", "⁴", "⁵", "⁶", "⁷", "⁸", "⁹"];
 /** Converts the given number to a string of superscript numbers. Ex: 123 becomes "¹²³" */
 export function toSuperscript(value: number): string {
+	const SUPERSCRIPT_NUMBERS = ["⁰", "¹", "²", "³", "⁴", "⁵", "⁶", "⁷", "⁸", "⁹"];
 	return String(value).split("").map(char => SUPERSCRIPT_NUMBERS[+char]).join("");
 }
