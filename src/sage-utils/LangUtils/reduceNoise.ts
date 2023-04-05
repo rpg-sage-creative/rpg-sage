cb0f1a49e07ab27875928241c9a361bda7dc623e
Randal T Meyer <randal.t.meyer@gmail.com>
2023-04-05 14:16:33 -0700

    filter for isNotBlank as well

--- src/sage-utils/LangUtils/reduceNoise.ts
+++ src/sage-utils/LangUtils/reduceNoise.ts
@@ -1,9 +1,12 @@
 import { isUnique } from "../ArrayUtils";
+import { isNotBlank } from "../StringUtils";
 
-/** Removes noise words from the given terms and returns only unique terms. */
+/** Removes noise words from the given terms and returns only unique non-blank terms. */
 export function reduceNoise(terms: string[], additionalNoise = <string[]>[]): string[] {
 	/** The base set of noise words to remove. */
 	const NOISE = ["a", "and", "of", "the"];
 	const noise = NOISE.concat(additionalNoise);
-	return terms.filter(isUnique).filter(term => !noise.includes(term));
+	const notBlank = terms.filter(isNotBlank);
+	const unique = notBlank.filter(isUnique);
+	return unique.filter(term => !noise.includes(term));
 }
