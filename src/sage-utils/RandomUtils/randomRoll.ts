7198b6486fde3d0e5b1e5c4b9152dee0029f7b29
Randal T Meyer <randal.t.meyer@gmail.com>
2023-07-05 15:30:47 -0700

    streamlined signing use of random avoids min/max issues

--- src/sage-utils/RandomUtils/randomRoll.ts
+++ src/sage-utils/RandomUtils/randomRoll.ts
@@ -1,4 +1,4 @@
-import { generate } from "./generate";
+import { random } from "./random";
 
 /** Simple dice string formats in the form of XdY+-Z */
 export type TSimpleDice =
@@ -12,6 +12,13 @@ export type TSimpleDice =
 	| `+${number}d${number}-${number}`
 	| `+${number}d${number}+${number}`
 	;
+
+/**
+ * Sets the positive number's +/- sign to the given sign (used for adding to a total).
+ */
+function sign(value: number, sign: string): number {
+	return sign === "+" ? +value : -value;
+}
 
 /**
  * Returns the results of rolling simple dice: 1d6 or 1d8+1 or 1d10-2.
@@ -34,13 +41,9 @@ export function randomRoll(diceString: TSimpleDice): number | null {
 	const modifierSign = match[4] ?? "+";
 	const modifier = +(match[5] ?? 0);
 
-	let total = modifier * (modifierSign === "+" ? 1 : -1);
+	let total = sign(modifier, modifierSign);
 	for (let i = 0; i < diceCount; i++) {
-		if (diceSign === "+") {
-			total += generate(1, diceSides);
-		}else {
-			total -= generate(1, diceSides);
-		}
+		total += sign(random(diceSides), diceSign);
 	}
 	return total;
 }
