c567cead5a50ef884056191f6cbc0a56bb804bb5
Randal T Meyer <randal.t.meyer@gmail.com>
2023-07-05 15:07:04 -0700

    don't randomize if min/max are the same

--- src/sage-utils/RandomUtils/random.ts
+++ src/sage-utils/RandomUtils/random.ts
@@ -2,8 +2,10 @@ import { generate } from "./generate";
 
 /** Creates a random number between 1 and the given max (inclusive). */
 export function random(max: number): number;
-/** Creates a random number between then given min and max (inclusive). */
+/** Creates a random number between the given min and max (inclusive). */
 export function random(min: number, max: number): number;
 export function random(minOrMax: number, maxOrOne = 1): number {
-	return generate(Math.min(minOrMax, maxOrOne), Math.max(minOrMax, maxOrOne));
+	const min = Math.min(minOrMax, maxOrOne);
+	const max = Math.max(minOrMax, maxOrOne);
+	return min === max ? min : generate(min, max);
 }
