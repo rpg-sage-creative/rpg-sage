704dab4e65d362748678a29b7edf6a53c184071f
Randal T Meyer <randal.t.meyer@gmail.com>
2023-07-05 15:49:47 -0700

    use randomInt directly

--- src/sage-utils/RandomUtils/random.ts
+++ src/sage-utils/RandomUtils/random.ts
@@ -1,4 +1,4 @@
-import { generate } from "./generate";
+import { randomInt } from "crypto";
 
 /** Creates a random number between 1 and the given max (inclusive). */
 export function random(max: number): number;
@@ -7,5 +7,5 @@ export function random(min: number, max: number): number;
 export function random(minOrMax: number, maxOrOne = 1): number {
 	const min = Math.min(minOrMax, maxOrOne);
 	const max = Math.max(minOrMax, maxOrOne);
-	return min === max ? min : generate(min, max);
+	return min === max ? min : randomInt(min, max + 1);
 }
