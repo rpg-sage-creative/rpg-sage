0b5e4064a3e5f39c737b8ec100595c550302daa4
Randal T Meyer <randal.t.meyer@gmail.com>
2023-10-06 23:24:36 -0700

    tweaked to be usable on maps/sets

--- src/sage-utils/utils/TypeUtils/destroy.ts
+++ src/sage-utils/utils/TypeUtils/destroy.ts
@@ -1,18 +1,39 @@
 /**
+ * Attempts to call the given function on the given object.
+ * @returns true if the function was called, false otherwise.
+ */
+function call(object: any, fnName: "clear" | "destroy"): boolean {
+	if (typeof(object?.[fnName]) === "function") {
+		object[fnName]();
+		return true;
+	}
+	return false;
+}
+
+/**
+ * Attempts to call clear() or destroy() on the given object.
+ * @returns true if either function was called, false if neither was called.
+ */
+function clearOrDestroy(object: any): boolean {
+	const cleared = call(object, "clear");
+	const destroyed = call(object, "destroy");
+	return cleared || destroyed;
+}
+
+/**
  * Iterates the keys of the given object to remove references to help memory usage.
  * If a value has a clear() or destroy() function, they are called before the key is set to null.
  * @returns null for convenience, example: cache = destroy(cache);
  */
-export function destroy(core: any): null {
-	if (core) {
-		for (const key in core) {
-			if (typeof(core[key]?.clear) === "function") {
-				core[key].clear();
+export function destroy(object: any): null {
+	if (object) {
+		// try to clear/destroy the given object ...
+		if (!clearOrDestroy(object)) {
+			// ... else try to clear/destroy the object's properties
+			for (const key in object) {
+				clearOrDestroy(object[key]);
+				object[key] = null;
 			}
-			if (typeof(core[key]?.destroy) === "function") {
-				core[key].destroy();
-			}
-			core[key] = null;
 		}
 	}
 	return null;
