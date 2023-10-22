26f4e77cec094eef7cbefd1d993143819e73f9c0
Randal T Meyer <randal.t.meyer@gmail.com>
2023-10-22 11:51:22 -0700

    better comments, extra tests

--- src/sage-utils/utils/StringUtils/redactCodeBlocks.ts
+++ src/sage-utils/utils/StringUtils/redactCodeBlocks.ts
@@ -1,8 +1,11 @@
 import { matchCodeBlocks } from "./internal/matchCodeBlocks.js";
 
 /**
- * Converts any characters within `` blocks to asterisks. Ex: "a `code` block" becomes "a `****` block".
- * Matches 1, 2, or 3 ` characters.
+ * Converts any characters within back-tick (`) quoted blocks to asterisks.
+ * Ex: "a `code` block" becomes "a `****` block".
+ * Ignores back-tick characters that are preceded by a slash (\).
+ * Ex: " \`doesn't redact\` "
+ * Matches 1, 2, or 3 back-tick characters (because Discord's Markdown supports them).
 */
 export function redactCodeBlocks(content: string) {
 	// find all the matches
@@ -18,20 +21,21 @@ export function redactCodeBlocks(content: string) {
 	return content;
 }
 
+/** A handful of tests to ensure the logic is working. */
 export function redactCodeBlocksTest() {
 	const tests = [
 	//   input                          expected output
-		[" `redacted` ",               " `********` "],
-		[" \\`notredacted\\` ",        " \\`notredacted\\` "],
-		[" ``redacted`` ",             " ``********`` "],
-		[" \\``redacted`\\` ",         " \\``********`\\` "],
-		[" `\\`redacted\\`` ",         " `************` "],
-		[" ```redacted``` ",           " ```********``` "],
-		[" ```redacted\nredacted``` ", " ```*****************``` "],
-		[" \\```redacted``\\` ",       " \\```********``\\` "],
-		[" `\\``notredacted`\\`` ",    " `**`notredacted`**` "],
-		[" \\``\\`redacted``\\` ",     " \\``**********``\\` "],
-		[" \\``\\`redacted``\\` ",     " \\``**********``\\` "],
+		[" hi `redacted` no `shit` ",   " hi `********` no `****` "],
+		[" \\`notredacted\\` `hi` go ", " \\`notredacted\\` `**` go "],
+		["`0` ``redacted`` \\`9`",      "`*` ``********`` \\`9`"],
+		[" \\``redacted`\\` ",          " \\``********`\\` "],
+		[" `\\`redacted\\`` ",          " `************` "],
+		[" ```redacted``` ",            " ```********``` "],
+		[" ```redacted\nredacted``` ",  " ```*****************``` "],
+		[" \\```redacted``\\` ",        " \\```********``\\` "],
+		[" `\\``notredacted`\\`` ",     " `**`notredacted`**` "],
+		[" \\``\\`redacted``\\` ",      " \\``**********``\\` "],
+		[" \\``\\`redacted``\\` ",      " \\``**********``\\` "],
 	];
 	tests.forEach(([raw, expected]) => {
 		const actual = redactCodeBlocks(raw);
