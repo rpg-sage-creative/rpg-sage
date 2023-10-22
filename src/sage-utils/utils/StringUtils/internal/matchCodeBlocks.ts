cea7d8469a3c1951f30b2fc503e7314ef2dfb957
Randal T Meyer <randal.t.meyer@gmail.com>
2023-10-22 11:51:59 -0700

    negative lookbehind instead of reversing

--- src/sage-utils/utils/StringUtils/internal/matchCodeBlocks.ts
+++ src/sage-utils/utils/StringUtils/internal/matchCodeBlocks.ts
@@ -2,16 +2,18 @@ type CodeBlockMatch = {
 	index: number;
 	length: number;
 	redacted: string;
-	reversedIndex: number;
 	ticks: number;
 };
 
-function findNext(reversed: string, startIndex: number): CodeBlockMatch | null {
+/**
+ * @private Finds the next match in the string, starting from the given index
+ */
+function findNext(content: string, startIndex: number): CodeBlockMatch | null {
 	// let's work with a shorter string
-	const sliced = reversed.slice(startIndex);
+	const sliced = content.slice(startIndex);
 
 	// we only want ticks that aren't followed by slashes, min 1 max 3
-	const leftRegex = /(`(?!\\)){1,3}/;
+	const leftRegex = /((?<!\\)`){1,3}/;
 	const leftMatch = leftRegex.exec(sliced);
 	if (!leftMatch) {
 		return null;
@@ -22,27 +24,24 @@ function findNext(reversed: string, startIndex: number): CodeBlockMatch | null {
 
 	do {
 		// we try to grab as many ticks as we can
-		const rightRegex = new RegExp(`(\`(?!\\\\)){${ticks}}`);
+		const rightRegex = new RegExp(`((?<!\\\\)\`){${ticks}}`);
 
 		// we need to start out regex after the left ticks
 		const rightMatch = rightRegex.exec(sliced.slice(leftIndex + ticks));
 
 		if (rightMatch) {
-			/** final computed index of first tick in the reversed string */
-			const reversedIndex = startIndex + leftIndex;
+			/** final computed index of first tick */
+			const index = startIndex + leftIndex;
 
 			/** total length of match, including ticks */
 			const length = rightMatch.index + ticks * 2;
-
-			/** final computed index of first tick in the forward string */
-			const index = reversed.length - reversedIndex - length;
 
 			const ends = "".padEnd(ticks, "`");
 			const center = "".padEnd(rightMatch.index, "*");
 			const redacted = ends + center + ends;
 
 			// return match
-			return { index, length, redacted, reversedIndex, ticks };
+			return { index, length, redacted, ticks };
 		}
 
 		// we didn't get a match, so we try with fewer ticks
@@ -56,18 +55,18 @@ function findNext(reversed: string, startIndex: number): CodeBlockMatch | null {
 	return null;
 }
 
+/**
+ * @private Finds all the matches in a given string
+ */
 export function matchCodeBlocks(content: string): CodeBlockMatch[] {
-	// reverse the string for simpler regex of escaped back-ticks
-	const reversed = content.split("").reverse().join("");
-
-	// find all the matches
 	const matches: CodeBlockMatch[] = [];
-	let match: CodeBlockMatch | null;
+
 	let lastIndex = 0;
-	while (match = findNext(reversed, lastIndex)) {
+	let match: CodeBlockMatch | null;
+	while (match = findNext(content, lastIndex)) {
 		matches.push(match);
-		lastIndex = match.reversedIndex + match.length;
+		lastIndex = match.index + match.length;
 	}
 
-	return matches.reverse();
+	return matches;
 }
