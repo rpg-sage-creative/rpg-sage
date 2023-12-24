30315715edb95dbd03305d4f91b70299bf8858d2
Randal T Meyer <randal.t.meyer@gmail.com>
2023-12-24 01:27:46 -0800

    improve error messages and pin updating

--- src/sage-lib/sage/commands/admin/GameCharacter/gcCmdStats.ts
+++ src/sage-lib/sage/commands/admin/GameCharacter/gcCmdStats.ts
@@ -1,4 +1,5 @@
 import type SageMessage from "../../../model/SageMessage";
+import { updateSheet } from "../../pathbuilder";
 import { getCharacterForStats } from "./getCharacterForStats";
 import { getCharacterTypeMeta } from "./getCharacterTypeMeta";
 import { getUserDid } from "./getUserDid";
@@ -8,31 +9,36 @@ import { testCanAdminCharacter } from "./testCanAdminCharacter";
 export async function gcCmdStats(sageMessage: SageMessage): Promise<void> {
 	const characterTypeMeta = getCharacterTypeMeta(sageMessage);
 	if (!testCanAdminCharacter(sageMessage, characterTypeMeta)) {
-		return sageMessage.reactBlock();
+		return sageMessage.denyByPerm("Set Character Stats", "You aren't allowed to alter a character here.");
 	}
 
 	const userDid = await getUserDid(sageMessage),
 		names = sageMessage.args.removeAndReturnNames(),
 		character = await getCharacterForStats(sageMessage, characterTypeMeta, userDid!, names);
 
-	if (character) {
-		character.updateStats(sageMessage.args.keyValuePairs(), false);
-		return promptCharConfirm(sageMessage, character, `Update ${character.name}?`, async char => {
-			const charSaved = await char.save(true);
-			if (charSaved) {
-				const { game } = sageMessage;
-				if (game) {
-					game.encounters.updatePins();
-					game.parties.updatePins();
-					if (characterTypeMeta.isGm) {
-						game.updateGmCharacterName(char.name);
-						return game.save();
+	if (!character) {
+		await sageMessage.reactFailure(`${characterTypeMeta.singularDescriptor ?? "Character"} "${names.name}" not found.`);
+		return;
+	}
+
+	await character.updateStats(sageMessage.args.keyValuePairs(), false);
+	return promptCharConfirm(sageMessage, character, `Update ${character.name}?`, async char => {
+		const charSaved = await char.save(true);
+		if (charSaved) {
+			const { game } = sageMessage;
+			if (game) {
+				if (characterTypeMeta.isGm) {
+					game.updateGmCharacterName(char.name);
+					return game.save();
+				}else {
+					await game.encounters.updatePins();
+					await game.parties.updatePins();
+					if (char.pathbuilder?.messageId) {
+						await updateSheet(sageMessage.caches, char.pathbuilder);
 					}
 				}
-
 			}
-			return charSaved;
-		});
-	}
-	return sageMessage.reactFailure();
+		}
+		return charSaved;
+	});
 }
