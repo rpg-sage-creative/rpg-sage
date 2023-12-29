61c1a76f267a8c4dfc4cc9329323cf8586ba50db
Randal T Meyer <randal.t.meyer@gmail.com>
2023-12-28 16:01:08 -0800

    comment incomplete tokenize update

--- src/sage-utils/utils/StringUtils/tokenize.ts
+++ src/sage-utils/utils/StringUtils/tokenize.ts
@@ -1,11 +1,11 @@
-import { matchCodeBlocks } from "./internal/matchCodeBlocks";
+// import { matchCodeBlocks } from "./internal/matchCodeBlocks";
 
-export function tokenize(content: string, parsers: RegExp): [] {
-	//match redacted blocks
-	const matches = matchCodeBlocks(content);
-	//split the content around the redacted blocks
-	matches;
-	//tokenize the non-redacted blocks
-	parsers;
-	return [];
-}
+// export function tokenize(content: string, parsers: RegExp): [] {
+// 	//match redacted blocks
+// 	const matches = matchCodeBlocks(content);
+// 	//split the content around the redacted blocks
+// 	matches;
+// 	//tokenize the non-redacted blocks
+// 	parsers;
+// 	return [];
+// }
