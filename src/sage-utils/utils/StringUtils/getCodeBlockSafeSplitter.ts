60e0eef3354ec749937c45486806acc87807bcfa
Randal T Meyer <randal.t.meyer@gmail.com>
2024-01-02 11:51:54 -0800

    add code block safe string splitter

---
+++ src/sage-utils/utils/StringUtils/getCodeBlockSafeSplitter.ts
@@ -0,0 +1,26 @@
+import { codeBlockSafeSplit } from "./internal/codeBlockSafeSplit.js";
+
+type StringSplitter = {
+	/** @private contains the value to split on */
+	_splitter: string | RegExp;
+	/** the split symbol used by String.split */
+	[Symbol.split](value:string, limit?: number): string[];
+};
+
+/**
+ * Creates a string splitter that is capable of ignoring code blocks using backticks (`).
+ * Defaults to splitting on "\n".
+ */
+export function getCodeBlockSafeSplitter(): StringSplitter;
+export function getCodeBlockSafeSplitter(splitter: string | RegExp): StringSplitter;
+export function getCodeBlockSafeSplitter(splitter: string | RegExp = "\n"): StringSplitter {
+	return {
+		_splitter: splitter,
+		[Symbol.split](value: string, limit?: number): string[] {
+			if (value.includes("`")) {
+				return codeBlockSafeSplit(value, this._splitter, limit);
+			}
+			return value.split(this._splitter, limit);
+		}
+	};
+}
