83d9e0f87a5bd0ebd041f3d508946a0aa863bc7f
Randal T Meyer <randal.t.meyer@gmail.com>
2024-01-04 16:52:41 -0800

    stop using title (discord #, ##, ### is easier)

--- src/sage-lib/sage/commands/dialog/updateEmbed.ts
+++ src/sage-lib/sage/commands/dialog/updateEmbed.ts
@@ -1,9 +1,8 @@
 import { ColorResolvable, MessageEmbed } from "discord.js";
 import { Optional } from "../../../../sage-utils";
 
-export function updateEmbed(originalEmbed: MessageEmbed | undefined, title: Optional<string>, imageUrl: Optional<string>, content: string): MessageEmbed {
+export function updateEmbed(originalEmbed: MessageEmbed | undefined, imageUrl: Optional<string>, content: string): MessageEmbed {
 	const updatedEmbed = new MessageEmbed();
-	updatedEmbed.setTitle(title ?? originalEmbed?.title ?? "");
 	updatedEmbed.setDescription(content);
 	updatedEmbed.setThumbnail(imageUrl ?? originalEmbed?.thumbnail?.url ?? "");
 	updatedEmbed.setColor(originalEmbed?.color as ColorResolvable);
