162e9c98b70a621efaf13d19dbfe6575ec6fd70b
Randal T Meyer <randal.t.meyer@gmail.com>
2024-01-04 16:52:17 -0800

    smarter alias updating

--- src/sage-lib/sage/commands/dialog/updateAliasDialogArgsAndReturnType.ts
+++ src/sage-lib/sage/commands/dialog/updateAliasDialogArgsAndReturnType.ts
@@ -9,21 +9,22 @@ export function updateAliasDialogArgsAndReturnType(sageMessage: SageMessage, dia
 	}
 
 	const aliasContent = parseDialogContent(aliasFound.target)!;
+
+	const updatedAliasContent = (aliasContent.content ?? "")
+		.replace(/\\n/g, "<br/>");
+
 	const textRegex = /{text}/i;
-	const _content = (aliasContent.content ?? "").replace(/\\n/g, "<br/>");
-	const updatedContent = textRegex.test(_content)
-		? _content.replace(textRegex, dialogContent.content)
-		: _content + dialogContent.content;
+	const updatedContent = textRegex.test(updatedAliasContent)
+		? updatedAliasContent.replace(textRegex, dialogContent.content)
+		: updatedAliasContent + dialogContent.content;
 
 	return {
 		type: aliasContent.type,
 		// alias: undefined,
-		// who: undefined,
 		attachment: dialogContent.attachment ?? aliasContent.attachment,
 		postType: dialogContent.postType ?? aliasContent.postType,
-		name: dialogContent.who ?? aliasContent.name,
-		displayName: aliasContent.displayName,
-		title: dialogContent.title ?? aliasContent.title,
+		name: dialogContent.name ?? aliasContent.name,
+		displayName: dialogContent.displayName ?? aliasContent.displayName,
 		imageUrl: dialogContent.imageUrl ?? aliasContent.imageUrl,
 		embedColor: dialogContent.embedColor ?? aliasContent.embedColor,
 		content: updatedContent
