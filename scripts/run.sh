dd67f3d039cd96b9a456a80eb49510842d4d2602
Randal T Meyer <randal.t.meyer@gmail.com>
2024-01-11 22:23:06 -0800

    fix run script for aws/docker

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,6 +7,11 @@ bash scripts/scrub.sh pm2
 
 bash scripts/build.sh
 
+DATA_ROOT="/home/ec2-user/legacy/data"
+if [ -d "/Users/randaltmeyer/git/rpg-sage-legacy-data" ]; then
+	DATA_ROOT="/Users/randaltmeyer/git/rpg-sage-legacy-data"
+fi
+
 if [ "$1" = "pm2" ]; then
 
 	pm2 start mono.config.cjs --env dev
@@ -18,7 +23,7 @@ else
 		--es-module-specifier-resolution=node \
 		mono.mjs \
 		botCodeName=dev \
-		dataRoot=/Users/randaltmeyer/git/rpg-sage-legacy-data \
+		"dataRoot=$DATA_ROOT" \
 		superUserId=253330271678627841 \
 		homeServerId=963531189254238278
 
