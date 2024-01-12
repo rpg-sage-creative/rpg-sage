abef911bd51b25f7e9d52d8c3acb0155112eecba
Randal T Meyer <randal.t.meyer@gmail.com>
2024-01-12 14:04:43 -0800

    check root and parent folders for build.json

--- src/sage-utils/utils/EnvUtils/getBuildInfo.ts
+++ src/sage-utils/utils/EnvUtils/getBuildInfo.ts
@@ -13,23 +13,32 @@ type BuildInfo = {
 	author: string;
 };
 
+let buildInfo: BuildInfo;
+
 export function getBuildInfo(): BuildInfo {
-	const path = "../build.json";
-	try {
-		return JSON.parse(readFileSync(path).toString());
-	}catch(ex) {
-		console.error(`Invalid Build Info: ${path}`);
+	if (!buildInfo) {
+		const paths = ["./build.json", "../build.json"];
+		for (const path of paths) {
+			try {
+				buildInfo = JSON.parse(readFileSync(path).toString());
+			}catch(ex) {
+				// ignore
+			}
+		}
 	}
-	return {
-		buildTs: 0,
-		buildDate: "1978-12-24",
-		buildTime: "1945",
-		branch: "mystery",
-		commit: "",
-		commitSubject: "unknown",
-		commitTs: 0,
-		commitDate: "1978-12-24",
-		commitTime: "1945",
-		author: "Randal T Meyer"
-	};
+	if (!buildInfo) {
+		buildInfo = {
+			buildTs: 0,
+			buildDate: "1978-12-24",
+			buildTime: "1945",
+			branch: "mystery",
+			commit: "",
+			commitSubject: "unknown",
+			commitTs: 0,
+			commitDate: "1978-12-24",
+			commitTime: "1945",
+			author: "Randal T Meyer"
+		};
+	}
+	return buildInfo;
 }
