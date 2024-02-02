09f55aafcf3672959c712cd4262b5ce3b91e9dc3
Randal T Meyer <randal.t.meyer@gmail.com>
2024-02-02 11:59:45 -0800

    add test-utils as package

--- test/index.mjs
+++ test/index.mjs
@@ -1,7 +1,8 @@
+import { error } from "@rsc-utils/console-utils";
+import { runTests } from "@rsc-utils/test-utils";
 
 // node --experimental-modules --es-module-specifier-resolution=node run.mjs
 
-async function main() {
-	console.log(`Create rpg-sage-legacy tests`);
-}
-main();
+runTests(async function testRpgSageLegacy() {
+	error(`Create rpg-sage-legacy tests`);
+}, true);
