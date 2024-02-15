2e4b1a3c2cdf6f42640277db7be272b2f618389a
Randal T Meyer <randal.t.meyer@gmail.com>
2024-02-14 17:03:35 -0800

    update CnC dice

--- src/sage-dice/dice/cnc/index.ts
+++ src/sage-dice/dice/cnc/index.ts
@@ -1,19 +1,8 @@
-import { DiceExplode, DiceTest, DiceTestType, DieRollGrade, DicePart as baseDicePart, DicePartRoll as baseDicePartRoll, gradeToEmoji, parseDiceTestTargetValue, rollDice, type DiceTestData, type DicePartCore as baseDicePartCore, type DicePartRollCore as baseDicePartRollCore, type DicePartCoreArgs as baseTDicePartCoreArgs } from "@rsc-utils/dice-utils";
+import { Dice, DiceExplode, DiceGroup, DicePart, DiceTest, DiceTestType, DieRollGrade, UNICODE_LEFT_ARROW, cleanDicePartDescription, gradeToEmoji, parseDiceTestTargetValue, type DiceBase, type DiceCore, type DiceGroupCore, type DiceGroupCoreArgs, type DicePartCore, type DicePartCoreArgs, type DiceTestData, type TDice, type TDiceGroup, type TDicePart } from "@rsc-utils/dice-utils";
 import { randomSnowflake } from "@rsc-utils/snowflake-utils";
-import { cleanWhitespace, tokenize, type TokenData, type TokenParsers } from "@rsc-utils/string-utils";
-import { type OrNull } from "@rsc-utils/type-utils";
+import { cleanWhitespace, type TokenData, type TokenParsers } from "@rsc-utils/string-utils";
 import { GameType } from "../../../sage-common";
-import { DiceOutputType, DiceSecretMethodType, TDiceLiteral, UNICODE_LEFT_ARROW, cleanDescription } from "../../common";
-import {
-	Dice as baseDice, DiceGroup as baseDiceGroup,
-	DiceGroupRoll as baseDiceGroupRoll,
-	DiceRoll as baseDiceRoll
-} from "../base";
-import type {
-	DiceCore as baseDiceCore, DiceGroupCore as baseDiceGroupCore,
-	DiceGroupRollCore as baseDiceGroupRollCore,
-	DiceRollCore as baseDiceRollCore
-} from "../base/types";
+import { DiceSecretMethodType } from "../../common";
 
 /*
 default target ("VS") = 8
@@ -38,10 +27,10 @@ final success/failure
 
 //#region Tokenizer
 
-function getParsers(): TokenParsers {
+function getTokenParsers(): TokenParsers {
 	return {
 		dice: /\s*(\d+)?\s*d\s*(12)/i,
-		target: /(vs)\s*(\d+)/i
+		target: /(vs)\s*(\d+|\|\|\d+\|\|)/i
 	};
 }
 
@@ -64,12 +53,11 @@ function reduceTokenToDicePartCore<T extends DicePartCore>(core: T, token: Token
 
 enum TargetType { None = 0, VS = 1 }
 
-type TTargetData = { type:TargetType; value:number; hidden:boolean; };
-
-function targetDataToTestData(targetData: TTargetData): OrNull<DiceTestData> {
-	if (!targetData) return null;
-	const { value, hidden } = targetData;
-	return DiceTest.createData(DiceTestType.GreaterThanOrEqual, value, hidden, "vs");
+function targetDataToTestData(targetData?: DiceTestData<TargetType>): DiceTestData | undefined {
+	if (targetData) {
+		return DiceTest.createData(DiceTestType.GreaterThanOrEqual, targetData.value, targetData.hidden, "vs");
+	}
+	return undefined;
 }
 
 //#endregion
@@ -142,241 +130,96 @@ function gradeRoll(baseValues: number[], explodedValues: number[], vs: number): 
 	});
 
 	const totalSuccesses = values[0] + values[5];
-	const grade = totalSuccesses === 0 ? DieRollGrade.Failure
-		: totalSuccesses < 0 ? DieRollGrade.CriticalFailure
-		: DieRollGrade.Success;
+	let grade: DieRollGrade;
+	if (totalSuccesses === 0) {
+		grade = DieRollGrade.Failure;
+	}else if (totalSuccesses < 0) {
+		grade = DieRollGrade.CriticalFailure;
+	}else {
+		grade = DieRollGrade.Success;
+	}
 
 	return [grade, values];
 }
 
 //#endregion
 
-//#region DicePart
+export class CnCDicePart extends DicePart<DicePartCore<TargetType>, TargetType, GameType> {
 
-interface DicePartCore extends baseDicePartCore {
-	target?: TTargetData;
-}
-
-type TDicePartCoreArgs = baseTDicePartCoreArgs & {
-	testOrTarget?: DiceTestData | TTargetData;
-};
-
-export class DicePart extends baseDicePart<DicePartCore, DicePartRoll> {
-	//#region static
-	public static create({ count, description, testOrTarget }: TDicePartCoreArgs = {}): DicePart {
-		return new DicePart({
+	public static create<DicePartType extends TDicePart>({ count, description, target }: DicePartCoreArgs = {}): DicePartType {
+		return new this({
 			objectType: "DicePart",
-			gameType: GameType.CnC,
+			gameType: this.GameType,
 			id: randomSnowflake(),
 
+			children: undefined!,
 			count: count ?? 1,
-			description: cleanDescription(description),
-			// manipulation: undefined,
+			description: cleanDicePartDescription(description),
+			fixedRolls: undefined!,
+			manipulation: [{ explode:new DiceExplode({ alias:"x", type:DiceTestType.Equal, value:12 }) }],
 			modifier: 0,
 			sides: 12,
-			// sign: undefined,
-			test: targetDataToTestData(testOrTarget as TTargetData) ?? testOrTarget as DiceTestData ?? null,
-			target: testOrTarget as TTargetData ?? null
-		});
+			sign: undefined!,
+			test: this.targetDataToTestData(target),
+			target
+		}) as DicePartType;
 	}
-	public static fromCore(core: DicePartCore): DicePart {
-		return new DicePart(core);
-	}
-	public static fromTokens(tokens: TokenData[]): DicePart {
-		const core = tokens.reduce(reduceTokenToDicePartCore, <DicePartCore>{ description:"" });
-		const args = <TDicePartCoreArgs>{ testOrTarget:core.target ?? core.test, ...core };
-		return DicePart.create(args);
-	}
-	//#endregion
+
+	public static readonly reduceTokenToCore = reduceTokenToDicePartCore;
+
+	public static readonly targetDataToTestData = targetDataToTestData;
+
+	public static readonly GameType = GameType.CnC;
+
 }
 
-//#endregion
-
-//#region DicePartRoll
-
-type DicePartRollCore = baseDicePartRollCore;
-
-export class DicePartRoll extends baseDicePartRoll<DicePartRollCore, DicePart> {
-	//#region static
-	public static create(dicePart: DicePart): DicePartRoll {
-		return new DicePartRoll({
-			objectType: "DicePartRoll",
-			gameType: GameType.CnC,
-			id: randomSnowflake(),
-			dice: dicePart.toJSON(),
-			rolls: rollDice(dicePart.count, dicePart.sides)
-		});
-	}
-	public static fromCore(core: DicePartRollCore): DicePartRoll {
-		return new DicePartRoll(core);
-	}
-	public static Dice = <typeof baseDicePart>DicePart;
-	//#endregion
-}
-
-DicePart.Roll = <typeof baseDicePartRoll>DicePartRoll;
-
-//#endregion
-
-//#region Dice
-
-type DiceCore = baseDiceCore;
-
-export class Dice extends baseDice<DiceCore, DicePart, DiceRoll> {
-	//#region static
-	public static create(diceParts: DicePart[]): Dice {
-		return new Dice({
-			objectType: "Dice",
-			gameType: GameType.CnC,
-			id: randomSnowflake(),
-			diceParts: diceParts.map<DicePartCore>(Dice.toJSON)
-		});
-	}
-	public static fromCore(core: DiceCore): Dice {
-		return new Dice(core);
-	}
-	public static fromDicePartCores(dicePartCores: DicePartCore[]): Dice {
-		return Dice.create(dicePartCores.map(DicePart.fromCore));
-	}
-	public static parse(diceString: string): Dice {
-		const diceGroup = DiceGroup.parse(diceString);
-		return diceGroup && diceGroup.dice[0] || null;
-	}
-	public static roll(diceString: TDiceLiteral): number;
-	public static roll(diceString: string): OrNull<number>;
-	public static roll(diceString: string): OrNull<number> {
-		const _dice = Dice.parse(diceString);
-		return _dice?.quickRoll() ?? null;
-	}
-
-	public static Part = <typeof baseDicePart>DicePart;
-	//#endregion
-}
-
-//#endregion
-
-//#region DiceRoll
-
-type DiceRollCore = baseDiceRollCore;
-
-export class DiceRoll extends baseDiceRoll<DiceRollCore, Dice, DicePartRoll> {
-	public toString(): string {
-		const baseValues = this.rolls[0].rolls;
-		const critValues = this.rolls[1]?.rolls ?? [];
-		const vs = this.dice.test?.value ?? 8;
+export class CnCDice extends Dice<DiceCore, CnCDicePart, GameType> {
+	public toRollString(): string {
+		const sortedRollData = this.primary?.sortedRollData;
+		const baseCount = sortedRollData?.initialCount ?? 0;
+		const baseValues = sortedRollData?.byIndex.slice(0, baseCount).map(r => r.value) ?? [];
+		const critValues = sortedRollData?.byIndex.slice(baseCount).map(r => r.value) ?? [];
+		const vs = this.test?.value ?? 8;
 		const vsOutput = vs !== 8 ? ` vs ${vs} ` : ``;
 		const [grade, gradeValues] = gradeRoll(baseValues, critValues, vs);
 		const gradeOutput = gradeToEmoji(grade);
 		const baseOutput = ` [${baseValues.join(",")}]${baseValues.length}d12 ${vsOutput} (**${gradeValues[0]}**)`;
 		const critOutput = critValues.length ? ` + [${critValues.join(",")}]${critValues.length}d12 ${vsOutput} (**${gradeValues[5]}**)` : ``;
 		const totalSuccesses = critValues.length ? ` -> **${gradeValues[0] + gradeValues[5]}**` : ``;
-		const desc = this.dice.diceParts.find(dp => dp.hasDescription)?.description;
+		const desc = this.children.find(dp => dp.hasDescription)?.description;
 		const descOutput = desc ? "`" + desc + "`" : "";
 		return cleanWhitespace(`${gradeOutput} ${descOutput} ${UNICODE_LEFT_ARROW} ${baseOutput} ${critOutput} ${totalSuccesses}`);
 	}
 
-	//#region static
-	public static create(_dice: Dice): DiceRoll {
-		const dicePartsRolls = _dice.diceParts.map(dicePart => dicePart.roll());
-		const dicePartsRollsJson = dicePartsRolls.map(dicePartRoll => dicePartRoll.toJSON());
-		const explodedDice: number[] = [];
-		dicePartsRolls.forEach(dicePartRoll => {
-			explodedDice.push(...DiceExplode.explode(12, dicePartRoll.rolls));
-		});
-		if (explodedDice.length) {
-			dicePartsRollsJson.push({
-				objectType: "DicePartRoll",
-				gameType: GameType.CnC,
-				id: randomSnowflake(),
-				dice: {
-					objectType: "DicePart",
-					gameType: GameType.CnC,
-					id: randomSnowflake(),
-					count: explodedDice.length,
-					sides: 12,
-					description: "Crit Dice",
-					modifier: 0
-				},
-				rolls: explodedDice
-			});
-		}
-		return new DiceRoll({
-			objectType: "DiceRoll",
-			gameType: GameType.CnC,
-			id: randomSnowflake(),
-			dice: _dice.toJSON(),
-			rolls: dicePartsRollsJson
-		});
-	}
-	public static fromCore(core: DiceRollCore): DiceRoll {
-		return new DiceRoll(core);
-	}
-	public static Dice = <typeof baseDice>Dice;
-	//#endregion
+	public static readonly Child = CnCDicePart as typeof DiceBase;
+
+	public static readonly GameType = GameType.CnC;
+
+	// We shouldn't be using this, but just in case let's return unknown.
+	public static readonly gradeRoll = () => 0;
+
+	public static readonly gradeToEmoji = gradeToEmoji;
 }
 
-Dice.Roll = <typeof baseDiceRoll>DiceRoll;
+export class CnCDiceGroup extends DiceGroup<DiceGroupCore, CnCDice, GameType> {
 
-//#endregion
-
-//#region DiceGroup
-
-type DiceGroupCore = baseDiceGroupCore;
-
-export class DiceGroup extends baseDiceGroup<DiceGroupCore, Dice, DiceGroupRoll> {
-	//#region static
-	public static create(_dice: Dice[], diceOutputType?: DiceOutputType): DiceGroup {
-		return new DiceGroup({
+	public static create<DiceGroupType extends TDiceGroup, DiceType extends TDice>(dice: DiceType[], args?: DiceGroupCoreArgs): DiceGroupType {
+		return new this({
 			objectType: "DiceGroup",
-			gameType: GameType.CnC,
+			gameType: this.GameType,
 			id: randomSnowflake(),
-			critMethodType: undefined,
-			dice: _dice.map<DiceCore>(DiceGroup.toJSON),
-			diceOutputType: diceOutputType,
-			diceSecretMethodType: DiceSecretMethodType.Ignore
-		});
+
+			children: dice.map(d => d.toJSON()),
+			criticalMethodType: undefined!,
+			outputType: args?.outputType,
+			secretMethodType: DiceSecretMethodType.Ignore,
+		}) as DiceGroupType;
 	}
-	public static fromCore(core: DiceGroupCore): DiceGroup {
-		return new DiceGroup(core);
-	}
-	public static fromTokens(tokens: TokenData[], diceOutputType?: DiceOutputType): DiceGroup {
-		return DiceGroup.create([Dice.create([DicePart.fromTokens(tokens)])], diceOutputType);
-	}
-	public static parse(diceString: string, diceOutputType?: DiceOutputType): DiceGroup {
-		const tokens = tokenize(diceString, getParsers(), "desc");
-		return DiceGroup.fromTokens(tokens, diceOutputType);
-	}
-	public static Part = <typeof baseDice>Dice;
-	public static Roll: typeof baseDiceGroupRoll;
-	//#endregion
+
+	public static readonly getTokenParsers = getTokenParsers;
+
+	public static readonly Child = CnCDice as typeof DiceBase;
+
+	public static readonly GameType = GameType.CnC;
+
 }
-
-//#endregion
-
-//#region DiceGroupRoll
-
-type DiceGroupRollCore = baseDiceGroupRollCore;
-
-export class DiceGroupRoll extends baseDiceGroupRoll<DiceGroupRollCore, DiceGroup, DiceRoll> {
-	public toString(outputType: DiceOutputType): string;
-	public toString(): string {
-		return this.rolls[0].toString();
-	}
-	public static create(diceGroup: DiceGroup): DiceGroupRoll {
-		return new DiceGroupRoll({
-			objectType: "DiceGroupRoll",
-			gameType: GameType.CnC,
-			id: randomSnowflake(),
-			diceGroup: diceGroup.toJSON(),
-			rolls: diceGroup.dice.map(_dice => _dice.roll().toJSON())
-		});
-	}
-	public static fromCore(core: DiceGroupRollCore): DiceGroupRoll {
-		return new DiceGroupRoll(core);
-	}
-	public static Dice = <typeof baseDiceGroup>DiceGroup;
-}
-
-DiceGroup.Roll = <typeof baseDiceGroupRoll>DiceGroupRoll;
-
-//#endregion
