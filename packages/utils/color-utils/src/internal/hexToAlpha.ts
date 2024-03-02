4a205b44f32f70b4b7ee7e305dc8bf894f49b450
Randal T Meyer <randal.t.meyer@gmail.com>
2024-03-01 16:35:04 -0800

    moved to submodule

--- packages/utils/color-utils/src/internal/hexToAlpha.ts
+++
@@ -1,6 +0,0 @@
-import { round } from "@rsc-utils/number-utils";
-
-/** Converts a Hex based alpha to a decimal number (between 0-1) value */
-export function hexToAlpha(value: string): number {
-	return round(parseInt(value, 16) / 255, 3);
-}
