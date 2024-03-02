22113a06d39a02988b1097767194ec02f9a0392e
Randal T Meyer <randal.t.meyer@gmail.com>
2024-03-01 16:35:08 -0800

    moved to submodule

--- packages/utils/math-utils/src/asinDeg.ts
+++
@@ -1,3 +0,0 @@
-export function asinDeg(deg: number): number {
-	return Math.asin(deg) * 360 / (2 * Math.PI);
-}
--- packages/utils/math-utils/src/cosDeg.ts
+++
@@ -1,3 +0,0 @@
-export function cosDeg(deg: number): number {
-	return Math.cos(deg * 2.0 * Math.PI / 360);
-}
