63ed7dfa840f5b06fd1cf0d479c7caf98ad3bb42
Randal T Meyer <randal.t.meyer@gmail.com>
2024-03-01 16:35:07 -0800

    moved to submodule

--- packages/utils/env-utils/src/internal/isValidId.ts
+++
@@ -1,5 +0,0 @@
-import { type Snowflake, isNonNilSnowflake } from "@rsc-utils/snowflake-utils";
-
-export function isValidId(value: string | number | null | undefined): value is Snowflake {
-	return isNonNilSnowflake(String(value));
-}
