1deb3bb037a7bf460d897ca7c1198b553df4df17
Randal T Meyer <randal.t.meyer@gmail.com>
2024-03-01 16:35:06 -0800

    moved to submodule

--- packages/utils/enum-utils/src/getEnumValues.ts
+++
@@ -1,6 +0,0 @@
-import { getEnumKeys } from "./getEnumKeys.js";
-
-/** Used to get the values of an enum. Use the following syntax: values<Enum>(Enum); */
-export function getEnumValues<T>(_enum: any): T[] {
-	return getEnumKeys(_enum).map(key => _enum[key] as T);
-}
