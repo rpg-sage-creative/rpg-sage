b5b6997b2a49f27ca3466f163c542ea3c9bc110a
Randal T Meyer <randal.t.meyer@gmail.com>
2024-03-01 16:35:07 -0800

    moved to submodule

--- packages/utils/https-utils/src/getProtocol.ts
+++
@@ -1,6 +0,0 @@
-import followRedirects from "follow-redirects";
-
-/** Returns http if the url starts with http://, or https otherwise. */
-export function getProtocol(url: string): typeof followRedirects.http | typeof followRedirects.https {
-	return (/^http:\/\//i).test(url) ? followRedirects.http : followRedirects.https;
-}
