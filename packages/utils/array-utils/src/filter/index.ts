c290555b4f72770f1aef9e5e8e6486f1067ed235
Randal T Meyer <randal.t.meyer@gmail.com>
2024-03-01 16:35:03 -0800

    moved to submodule

--- packages/utils/array-utils/src/filter/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./and.js";
-export * from "./Filter.js";
-export * from "./toUnique.js";
-export * from "./toUniqueDefined.js";
--- packages/utils/array-utils/src/sort/Comparable.ts
+++
@@ -1,5 +0,0 @@
-import type { SortResult } from "./SortResult.js";
-
-export interface Comparable<T> {
-	compareTo(other: Comparable<T>): SortResult;
-}
