08ac4a7022267ffea0c957c57538522418d4fa79
Randal T Meyer <randal.t.meyer@gmail.com>
2024-03-01 16:35:19 -0800

    moved to submodule

--- packages/utils/snowflake-utils/src/isNonNilSnowflake.ts
+++
@@ -1,9 +0,0 @@
-import type { Optional } from "@rsc-utils/type-utils";
-import { isNilSnowflake } from "./isNilSnowflake.js";
-import { isSnowflake } from "./isSnowflake.js";
-import type { Snowflake } from "./types.js";
-
-/** Returns true if the value is a valid non-nil Snowflake. */
-export function isNonNilSnowflake(value: Optional<Snowflake>): value is Snowflake {
-	return isSnowflake(value) && !isNilSnowflake(value);
-}
--- packages/utils/snowflake-utils/src/orNilSnowflake.ts
+++
@@ -1,9 +0,0 @@
-import type { Optional } from "@rsc-utils/type-utils";
-import { isNilSnowflake } from "./isNilSnowflake.js";
-import { isSnowflake } from "./isSnowflake.js";
-import { NIL_SNOWFLAKE, type Snowflake } from "./types.js";
-
-/** Returns the value if it is a valid Snowflake, otherwise it returns NIL_SNOWFLAKE. */
-export function orNilSnowflake(value: Optional<Snowflake>): Snowflake {
-	return isSnowflake(value) && !isNilSnowflake(value) ? value : NIL_SNOWFLAKE;
-}
