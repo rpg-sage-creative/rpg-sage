c58922cd216d6cb910705a5fac24f96c81ba8a5c
Randal T Meyer <randal.t.meyer@gmail.com>
2024-03-09 16:06:24 -0800

    update script to have beta/stable args

--- scripts/slash-payload.sh
+++ scripts/slash-payload.sh
@@ -6,11 +6,16 @@
 cd build
 
 update=
-if [ "$1" = "update" ]; then update="update"; fi
+if [ "$1" = "update" ] || [ "$2" = "update" ]; then update="update"; fi
+
+which="dev"
+if [ "$1" = "beta" ] || [ "$2" = "beta" ]; then which="beta"; fi
+if [ "$1" = "stable" ] || [ "$2" = "stable" ]; then which="stable"; fi
+
 
 node --experimental-modules \
 	--es-module-specifier-resolution=node \
 	slash.mjs \
 	"$update" \
-	botCodeName=dev \
+	"botCodeName=$which" \
 	dataRoot=/Users/randaltmeyer/git/rpg-sage-legacy-data
