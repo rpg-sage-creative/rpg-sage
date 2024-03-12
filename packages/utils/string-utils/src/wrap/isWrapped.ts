ac5c10e0b2a5453f9b57e06c77f67243f60f2217
Randal T Meyer <randal.t.meyer@gmail.com>
2024-03-12 10:51:22 -0700

    update isWrapped

--- packages/utils/string-utils/src/wrap/isWrapped.ts
+++ packages/utils/string-utils/src/wrap/isWrapped.ts
@@ -1,12 +1,11 @@
 import { splitChars } from "./splitChars.js";
 
 /**
- * Returns the characters wrapping the content, usually (), [], {}, or <>.
- * Returns false if the content is not wrapped by the given characters.
+ * Returns true if the input has characters wrapped by the given characters, false otherwise.
  * If the chars argument is even, then they are split and used as left/right.
  * If the chars argument is odd, then they are uesd as left and then they are reversed and used as right.
  */
 export function isWrapped(input: string, chars: string): boolean {
 	const { left, right } = splitChars(chars);
-	return input.startsWith(left) && input.endsWith(right);
+	return input.length > left.length + right.length && input.startsWith(left) && input.endsWith(right);
 }
