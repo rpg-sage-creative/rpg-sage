c62af0a066ec415155ed63728f60da6ac1fcc5dc
Randal T Meyer <randal.t.meyer@gmail.com>
2024-03-12 22:17:54 -0700

    add unified option

--- scripts/app-commands.sh
+++ scripts/app-commands.sh
@@ -6,12 +6,15 @@
 cd build
 
 update=
-if [ "$1" = "update" ] || [ "$2" = "update" ]; then update="update"; fi
+if [ "$1" = "update" ] || [ "$2" = "update" ] || [ "$3" = "update" ]; then update="update"; fi
+
+unified=
+if [ "$1" = "unified" ] || [ "$2" = "unified" ] || [ "$3" = "unified" ]; then unified="unified"; fi
 
 which="dev"
-if [ "$1" = "beta" ] || [ "$2" = "beta" ]; then which="beta"; fi
-if [ "$1" = "stable" ] || [ "$2" = "stable" ]; then which="stable"; fi
+if [ "$1" = "beta" ] || [ "$2" = "beta" ] || [ "$3" = "beta" ]; then which="beta"; fi
+if [ "$1" = "stable" ] || [ "$2" = "stable" ] || [ "$3" = "stable" ]; then which="stable"; fi
 
 
-node app-commands.mjs "$update" "botCodeName=$which" \
+node app-commands.mjs "$update" "$unified" "botCodeName=$which" \
 	dataRoot=/Users/randaltmeyer/git/rpg-sage-legacy-data
