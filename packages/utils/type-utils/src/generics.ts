5e89ef7f2eabe523efd8422d1ed558d68b8f2c6e
Randal T Meyer <randal.t.meyer@gmail.com>
2024-03-18 22:24:17 -0700

    add enumlike

--- packages/utils/type-utils/src/generics.ts
+++ packages/utils/type-utils/src/generics.ts
@@ -8,6 +8,12 @@ export type Awaitable<T> = T | PromiseLike<T>;
 /** Represents an unknown class constructor. */
 export type Constructable<T> = new (...args: any[]) => T;
 
+/** Represents an enum with string key and number value. */
+export type EnumLike<K extends string = string, V extends number = number> = Record<K, V>;
+
+/** Represents a conditional data type. */
+export type If<T extends boolean, A, B = undefined> = T extends true ? A : T extends false ? B : A | B;
+
 /** Represents an object that can be null or undefined. */
 export type Optional<T> = T | null | undefined;
 
@@ -16,6 +22,3 @@ export type OrNull<T> = T | null;
 
 /** Represents an object that can be undefined. */
 export type OrUndefined<T> = T | undefined;
-
-/** Represents a conditional data type. */
-export type If<T extends boolean, A, B = undefined> = T extends true ? A : T extends false ? B : A | B;
