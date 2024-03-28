67342a7b6c439317c0e22fd3e20e505b388c0339
Randal T Meyer <randal.t.meyer@gmail.com>
2024-03-27 22:12:14 -0700

    include gameType in updateSystemOptions

--- packages/sage/types/src/updates/updateSystemOptions.ts
+++ packages/sage/types/src/updates/updateSystemOptions.ts
@@ -4,11 +4,17 @@ import type { SystemOptions } from "../SageChannel.js";
 export type OldSystemOptions = SystemOptions & {
 	/** @deprecated */
 	defaultGameType?: number;
+	/** @deprecated */
+	gameType?: number;
 };
 
 export function updateSystemOptions(options: OldSystemOptions): void {
 	if (isDefined(options.defaultGameType)) {
 		options.gameSystemType = options.defaultGameType;
 		delete options.defaultGameType;
+	}
+	if (isDefined(options.gameType)) {
+		options.gameSystemType = options.gameType;
+		delete options.gameType;
 	}
 }
