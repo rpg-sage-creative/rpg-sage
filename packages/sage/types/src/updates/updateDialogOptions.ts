efa5346978161031d879f0ad6113217ff0f2e95e
Randal T Meyer <randal.t.meyer@gmail.com>
2024-03-27 22:57:23 -0700

    include defaultGmCharacterName in updateDialogOptions

--- packages/sage/types/src/updates/updateDialogOptions.ts
+++ packages/sage/types/src/updates/updateDialogOptions.ts
@@ -4,11 +4,17 @@ import type { DialogOptions } from "../SageChannel.js";
 export type OldDialogOptions = DialogOptions & {
 	/** @deprecated */
 	defaultDialogType?: number;
+	/** @deprecated */
+	defaultGmCharacterName?: string;
 };
 
 export function updateDialogOptions(options: OldDialogOptions): void {
 	if (isDefined(options.defaultDialogType)) {
 		options.dialogPostType = options.defaultDialogType;
 		delete options.defaultDialogType;
+	}
+	if (isDefined(options.defaultGmCharacterName)) {
+		options.gmCharacterName = options.defaultGmCharacterName;
+		delete options.defaultGmCharacterName;
 	}
 }
