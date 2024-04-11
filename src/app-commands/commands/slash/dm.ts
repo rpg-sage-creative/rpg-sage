7c13cba40a16f9b4a42da8520bddc82996451ef3
Randal T Meyer <randal.t.meyer@gmail.com>
2024-04-11 15:51:57 -0700

    update json

--- src/app-commands/commands/slash/dm.ts
+++ src/app-commands/commands/slash/dm.ts
@@ -2,7 +2,7 @@ import type { SlashCommand } from "../../types.js";
 
 export function registerCommand(): SlashCommand {
 	return {
-		"name": "DM",
-		"description": "Establish direct message channel with RPG Sage."
+		name: "DM",
+		description: "Establish direct message channel with RPG Sage."
 	};
 }
