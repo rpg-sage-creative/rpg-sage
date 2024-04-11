d79954ea1f3da365918629dd74740527a4034d95
Randal T Meyer <randal.t.meyer@gmail.com>
2024-04-11 15:52:26 -0700

    update game slash commands

--- src/app-commands/commands/slash/game.ts
+++ src/app-commands/commands/slash/game.ts
@@ -1,53 +1,51 @@
-import { DialogPostType, DiceCritMethodType, DiceOutputType, DicePostType, DiceSecretMethodType, GameType } from "@rsc-sage/types";
+import { DiceCritMethodType, DiceOutputType, GameType } from "@rsc-sage/types";
 import { getEnumKeys } from "@rsc-utils/enum-utils";
 import type { SlashCommand } from "../../types.js";
 
+function getCreateUpdateOptions(isUpdate: boolean) {
+	const options = [
+		{ name:"name", isRequired:!isUpdate },
+		{ name:"system", description:"Game System, ex: PF2e or DnD5e", choices:getEnumKeys(GameType) },
+		{ name:"ic", description:"In Character Channel" },
+		{ name:"ooc", description:"Out of Character Channel" },
+		{ name:"gm", description:"GM Only Channel" },
+		{ name:"misc", description:"Misc Channel" },
+		{ name:"gms", description:"Game Masters" },
+		{ name:"players", description:"Players" },
+		{ name:"dialogPost", choices:["Embed", "Post"] },
+		{ name:"gmCharName", description:"Game Master Character Name" },
+		{ name:"diceCrit", choices:getEnumKeys(DiceCritMethodType), description:"Critical Hit Method, ex: RollTwice, TimesTwo" },
+		{ name:"diceOutput", choices:getEnumKeys(DiceOutputType) },
+		{ name:"dicePost", choices:["Ignore", "Hide", { name:"GM Channel", value:"GameMasterChannel" }, { name:"GM Message", value:"GameMasterDirect" }] },
+		{ name:"diceSecret", choices:[{ name:"Post", value:"SinglePost" }, { name:"Embed", value:"SingleEmbed" }] },
+	];
+	if (isUpdate) {
+		options.push({ name:"remove", description:"Channels or Users to Remove from the Game" });
+	}
+	return options;
+}
+
 export function registerCommand(): SlashCommand {
-	const systemOption = { name:"system", description:"Game System, ex: PF2e or DnD5e", choices:getEnumKeys(GameType) };
-
-	const dialogPostOption = { name:"dialogPost", choices:getEnumKeys(DialogPostType) };
-
-	const diceCritOption = { name:"diceCrit", choices:getEnumKeys(DiceCritMethodType), description:"Critical Hit Method, ex: RollTwice, TimesTwo" };
-	const diceOutputOption = { name:"diceOutput", choices:getEnumKeys(DiceOutputType) };
-	const dicePostOption = { name:"dicePost", choices:getEnumKeys(DicePostType) };
-	const diceSecretOption = { name:"diceSecret", choices:getEnumKeys(DiceSecretMethodType) };
-
 	return {
 		name: "Game",
 		children: [
 			{
 				name: "Create",
-				description: "Creates a new RPG Sage Game",
-				options: [
-					{ name:"name", isRequired:true },
-					systemOption,
-					{ name:"ic", description:"In Character Channel" },
-					{ name:"ooc", description:"Out of Character Channel" },
-					{ name:"gm", description:"GM Only Channel" },
-					{ name:"misc", description:"Misc Channel" },
-					{ name:"gms", description:"Game Masters" },
-					{ name:"players", description:"Players" },
-					dialogPostOption,
-					{ name:"gmCharName", description:"Game Master Character Name" },
-					diceCritOption,
-					diceOutputOption,
-					dicePostOption,
-					diceSecretOption
-				]
+				description: "Create a new RPG Sage Game",
+				options: getCreateUpdateOptions(false),
+			},
+			{
+				name: "Details",
+				description: "View an RPG Sage Game",
 			},
 			{
 				name: "Update",
-				description: "Updates an RPG Sage Game",
-				options: [
-					{ name:"name" },
-					systemOption,
-					dialogPostOption,
-					{ name:"gmCharName", description:"Game Master Character Name" },
-					diceCritOption,
-					diceOutputOption,
-					dicePostOption,
-					diceSecretOption
-				]
+				description: "Update an RPG Sage Game",
+				options: getCreateUpdateOptions(true),
+			},
+			{
+				name: "Archive",
+				description: "Archive an RPG Sage Game",
 			}
 		]
 	};
