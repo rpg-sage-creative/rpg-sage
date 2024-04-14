ec6329bc73e5455b9e6510fd3b0f08f7b91bbd96
Randal T Meyer <randal.t.meyer@gmail.com>
2024-04-13 22:42:51 -0700

    add orgplayid to user

--- src/sage-lib/sage/commands/admin/user/user.ts
+++ src/sage-lib/sage/commands/admin/user/user.ts
@@ -1,33 +1,80 @@
 import { DialogPostType } from "@rsc-sage/types";
 import { toHumanReadable } from "@rsc-utils/discord-utils";
+import { registerListeners } from "../../../../discord/handlers/registerListeners.js";
+import type { SageCommand } from "../../../model/SageCommand.js";
 import type { SageMessage } from "../../../model/SageMessage.js";
 import type { User as SUser } from "../../../model/User.js";
 import { createAdminRenderableContent, registerAdminCommand } from "../../cmd.js";
 import { registerAdminCommandHelp } from "../../help.js";
 import { renderCount } from "../../helpers/renderCount.js";
 
-async function userCount(sageMessage: SageMessage): Promise<void> {
-	if (!sageMessage.isSuperUser) {
-		return sageMessage.reactBlock();
+async function userCount(sageCommand: SageCommand): Promise<void> {
+	if (sageCommand.isSuperUser) {
+		const users = await sageCommand.sageCache.users.getAll();
+		return renderCount(sageCommand, "Users", users.length);
 	}
-	const users = await sageMessage.sageCache.users.getAll();
-	return renderCount(sageMessage, "Users", users.length);
 }
+
+/**
+ * @todo include other organized play ids:
+ * "Cypher Play" (Monte Cook Games)
+ * https://dnd.wizards.com/adventurers-league
+ * https://www.chaosium.com/organized-play/
+ * https://www.facebook.com/groups/2757897967823921 (Frog God Games: https://discord.gg/dzpXfGG)
+ * https://stargatetherpg.com/files/file/44-stargate-phoenix-series-guide/
+ * https://www.shadowruntabletop.com/missions/
+ * https://fasagames.com/earthdawn-whatis/legendsofbarsaive/
+ * L5R has a fan run Org Play campaign, Heroes of Rokugan
+ * Kobold Press had something for their Midgard Campiagn it will probably carry over to Tales of the Valiant
+ * Evil Genius Games with Everyday Heroes: EGO (https://evilgeniusgames.com/the-organized-play/)
+ */
 
 async function userUpdate(sageMessage: SageMessage): Promise<void> {
-	if (!sageMessage.allowAdmin) {
-		return sageMessage.reactBlock();
+	if (!sageMessage.allowCommand) {
+		return sageMessage.denyByProv("User Update", "You cannot manage your settings here.");
 	}
-	const dialogPostType = sageMessage.args.getEnum(DialogPostType, "dialogPostType");
-	const sagePostType = sageMessage.args.getEnum(DialogPostType, "sagePostType");
-	const updated = await sageMessage.sageUser.update({ dialogPostType, sagePostType });
-	if (updated) {
+
+	const { validKeys, hasValidKeys, hasInvalidKeys } = sageMessage.args.validateKeys(["dialogPostType", "sagePostType", "orgPlayId"]);
+	if (!hasValidKeys || hasInvalidKeys) {
+		const details = [
+			"The command for updating your User settings is:",
+			"> ```sage!user update dialogPostType=\"\" sagePostType=\"\" orgPlayId=\"\"```",
+			"Acceptable PostType values are:",
+			"> `embed`, `post`, or `unset`",
+			"For example:",
+			"> ```sage!user update dialogPostType=\"embed\" sagePostType=\"post\"```",
+			"> ```sage!user update orgPlayId=\"999999\"```",
+			"> ```sage!user update dialogPostType=\"unset\" orgPlayId=\"unset\"```",
+		];
+		return sageMessage.whisper(details.join("\n"));
+	}
+
+	let opUpdated = false;
+	let ptUpdated = false;
+
+	if (validKeys.includes("orgPlayId")) {
+		const orgPlayId = sageMessage.args.getString("orgPlayId");
+		if (orgPlayId) {
+			opUpdated = await sageMessage.sageUser.notes.setUncategorizedNote("orgPlayId", orgPlayId);
+		}else {
+			opUpdated = await sageMessage.sageUser.notes.unsetUncategorizedNote("orgPlayId");
+		}
+	}
+
+	if (validKeys.includes("dialogPostType") || validKeys.includes("sagePostType")) {
+		const dialogPostType = sageMessage.args.getEnum(DialogPostType, "dialogPostType");
+		const sagePostType = sageMessage.args.getEnum(DialogPostType, "sagePostType");
+		ptUpdated = await sageMessage.sageUser.update({ dialogPostType, sagePostType });
+	}
+
+	if (opUpdated || ptUpdated) {
 		return userDetails(sageMessage);
 	}
-	return sageMessage.reactSuccessOrFailure(updated);
+
+	return sageMessage.reply("Sorry, we were unable to save your changes.", true);
 }
 
-async function userDetails(sageMessage: SageMessage): Promise<void> {
+async function userDetails(sageMessage: SageCommand): Promise<void> {
 	let user: SUser | null = sageMessage.sageUser;
 	if (sageMessage.isSuperUser) {
 		const userDid = sageMessage.args.getUserId("user");
@@ -43,7 +90,7 @@ async function userDetails(sageMessage: SageMessage): Promise<void> {
 		}
 	}
 
-	const renderableContent = createAdminRenderableContent(sageMessage.bot, `<b>user-details</b>`);
+	const renderableContent = createAdminRenderableContent(sageMessage.bot, `<b>User Details</b>`);
 	if (user) {
 		const discordUser = await sageMessage.discord.fetchUser(user.did);
 		if (discordUser) {
@@ -64,7 +111,7 @@ async function userDetails(sageMessage: SageMessage): Promise<void> {
 			renderableContent.append(`<b>Status</b> ${"<i>NOT FOUND</i>"}`);
 		}
 
-		renderableContent.append("");
+		renderableContent.append();
 		renderableContent.append(`<b>RPG Sage Id</b> ${user.id}`);
 
 		const dialogPostType = DialogPostType[user.dialogPostType!] ?? `<i>unset (Embed)</i>`;
@@ -73,20 +120,21 @@ async function userDetails(sageMessage: SageMessage): Promise<void> {
 		const sagePostType = DialogPostType[user.sagePostType!] ?? `<i>unset (Embed)</i>`;
 		renderableContent.append(`<b>Preferred Sage Post Type</b> ${sagePostType}`);
 
+		const orgPlayId = sageMessage.sageUser.notes.getUncategorizedNote("orgPlayId")?.note ?? `<i>unset</i>`;
+		renderableContent.append(`<b>Paizo Organized Play #</b> ${orgPlayId}`);
+
 		// TODO: List any games, gameRoles, servers, serverRoles!
 	} else {
 		renderableContent.append(`<blockquote>User Not Found!</blockquote>`);
 	}
-	return <any>sageMessage.send(renderableContent);
+
+	await sageMessage.reply(renderableContent, true);
 }
 
 export function registerUser(): void {
 	registerAdminCommand(userCount, "user-count");
 	registerAdminCommandHelp("Admin", "SuperUser", "User", "user count");
 
-	registerAdminCommand(userDetails, "user-details");
-	registerAdminCommandHelp("Admin", "SuperUser", "User", "user details *@UserMention*");
-	registerAdminCommandHelp("Admin", "SuperUser", "User", "user details *uuid*");
-
 	registerAdminCommand(userUpdate, "user-set", "user-update");
+	registerListeners({ commands:["user-details", "User Details"], interaction:userDetails, message:userDetails });
 }
