6b599d4e3e401591f88e6cec3afcc7e5d2b30b16
Randal T Meyer <randal.t.meyer@gmail.com>
2024-05-03 20:43:43 -0700

    remove help

--- src/sage-lib/sage/commands/admin/gameCharacter.ts
+++ src/sage-lib/sage/commands/admin/gameCharacter.ts
@@ -1,5 +1,4 @@
-import { registerAdminCommand } from "../cmd.js";
-import { registerAdminCommandHelp } from "../help.js";
+import { registerListeners } from "../../../discord/handlers/registerListeners.js";
 import { gcCmdAutoOff } from "./GameCharacter/gcCmdAutoOff.js";
 import { gcCmdAutoOn } from "./GameCharacter/gcCmdAutoOn.js";
 import { gcCmdCreate } from "./GameCharacter/gcCmdCreate.js";
@@ -9,38 +8,19 @@ import { gcCmdList } from "./GameCharacter/gcCmdList.js";
 import { gcCmdStats } from "./GameCharacter/gcCmdStats.js";
 import { gcCmdUpdate } from "./GameCharacter/gcCmdUpdate.js";
 
-function registerHelp(): void {
-	const AdminCategory = "Admin";
-	const playerCharacterSubCategory = "PC";
-	const nonPlayerCharacterSubCategory = "NPC";
-	const companionSubCategory = "Companion";
-	[playerCharacterSubCategory, nonPlayerCharacterSubCategory, companionSubCategory].forEach(subCat => {
-		const charName = subCat === companionSubCategory ? ` charname="character name"` : ``;
-		const cmd = subCat.toLowerCase();
-		registerAdminCommandHelp(AdminCategory, subCat, `${cmd} list "optional filter"`);
-		registerAdminCommandHelp(AdminCategory, subCat, `${cmd} details "name"`);
-		registerAdminCommandHelp(AdminCategory, subCat, `${cmd} create ${charName} name="name" avatar="image url" token="image url"`);
-		registerAdminCommandHelp(AdminCategory, subCat, `${cmd} update ${charName} name="name" avatar="image url"`);
-		registerAdminCommandHelp(AdminCategory, subCat, `${cmd} update ${charName} oldname="old name" newname="new name"`);
-		registerAdminCommandHelp(AdminCategory, subCat, `${cmd} delete ${charName} name="name"`);
-	});
-}
-
 export function registerGameCharacter(): void {
-	registerAdminCommand(gcCmdList, "pc-list", "pcs-list", "my-pc-list", "my-pcs");
-	registerAdminCommand(gcCmdList, "npc-list", "npcs-list", "my-npc-list", "my-npcs");
-	registerAdminCommand(gcCmdList, "companion-list", "my-companion-list", "my-companions");
-	registerAdminCommand(gcCmdList, "minion-list", "my-minion-list", "my-minions");
-	registerAdminCommand(gcCmdDetails, "pc-details", "npc-details", "companion-details", "gm-details", "minion-details");
-	registerAdminCommand(gcCmdCreate, `pc-create`, `npc-create`, `companion-create`, `minion-create`);
-	registerAdminCommand(gcCmdUpdate, "pc-update", "npc-update", "companion-update", "gm-update", "minion-update");
-	registerAdminCommand(gcCmdStats, "pc-stats", "npc-stats", "companion-stats", "minion-stats");
-	registerAdminCommand(gcCmdDelete, "pc-delete", "npc-delete", "companion-delete", "minion-delete");
-	registerAdminCommand(gcCmdAutoOn, "pc-auto-on", "gm-auto-on", "npc-auto-on", "minion-auto-on", "companion-auto-on");
-	registerAdminCommand(gcCmdAutoOff, "pc-auto-off", "gm-auto-off", "npc-auto-off", "minion-auto-off", "companion-auto-off");
-	// registerAdminCommand(characterLink, "pc-link", "npc-link", "companion-link", "minion-link");
-
-	registerHelp();
+	registerListeners({ commands:["pc|list", "pcs|list", "my|pc|list", "my|pcs"], message:gcCmdList });
+	registerListeners({ commands:["npc|list", "npcs|list", "my|npc|list", "my|npcs"], message:gcCmdList });
+	registerListeners({ commands:["companion|list", "my|companion|list", "my|companions"], message:gcCmdList });
+	registerListeners({ commands:["minion|list", "my|minion|list", "my|minions"], message:gcCmdList });
+	registerListeners({ commands:["pc|details", "npc|details", "companion|details", "gm|details", "minion|details"], message:gcCmdDetails });
+	registerListeners({ commands:[`pc|create`, `npc|create`, `companion|create`, `minion|create`], message:gcCmdCreate });
+	registerListeners({ commands:["pc|update", "npc|update", "companion|update", "gm|update", "minion|update"], message:gcCmdUpdate });
+	registerListeners({ commands:["pc|stats", "npc|stats", "companion|stats", "minion|stats"], message:gcCmdStats });
+	registerListeners({ commands:["pc|delete", "npc|delete", "companion|delete", "minion|delete"], message:gcCmdDelete });
+	registerListeners({ commands:["pc|auto|on", "gm|auto|on", "npc|auto|on", "minion|auto|on", "companion|auto|on"], message:gcCmdAutoOn });
+	registerListeners({ commands:["pc|auto|off", "gm|auto|off", "npc|auto|off", "minion|auto|off", "companion|auto|off"], message:gcCmdAutoOff });
+	// registerListeners({ commands:["pc-link", "npc-link", "companion-link", "minion-link"], message:characterLink });
 }
 
 // async function characterLink(sageMessage: SageMessage): Promise<void> {
