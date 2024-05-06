debd1c838c98f5b0de0b3744c2c85f7c4f80b463
Randal T Meyer <randal.t.meyer@gmail.com>
2024-05-06 14:08:37 -0700

    improved delete button logic and options

--- src/sage-lib/sage/model/utils/deleteButton.ts
+++ src/sage-lib/sage/model/utils/deleteButton.ts
@@ -1,34 +1,63 @@
 import { handleDiscordErrorReturnNull, type DMessage } from "@rsc-utils/discord-utils";
 import type { Optional } from "@rsc-utils/type-utils";
-import { MessageActionRow, MessageButton, type ButtonInteraction, type Snowflake } from "discord.js";
-import { registerInteractionListener } from "../../../discord/handlers";
-import type { SageInteraction } from "../SageInteraction";
+import { MessageActionRow, MessageButton, type ButtonInteraction, type MessageActionRowComponent, type Snowflake } from "discord.js";
+import { registerInteractionListener } from "../../../discord/handlers.js";
+import type { SageInteraction } from "../SageInteraction.js";
 
+export function createMessageDeleteButton(userId: Snowflake): MessageButton {
+	/** @todo update all Sage "delete" icons to use custom trashcan or wastebin emoji 🗑️ instead of ❌ */
+	return new MessageButton({
+		customId: `rpg-sage-message-delete-button-${userId}`,
+		style: "SECONDARY",
+		emoji: "❌",
+		label: "Delete this alert.",
+		type: "BUTTON"
+	});
+}
+
+/**
+ * Creates a MessageActionRow containing one Delete button.
+ * The button responds only the given user and deletes the message the button is attached to.
+ */
+export function createMessageDeleteButtonRow(userId: Snowflake): MessageActionRow<MessageActionRowComponent> {
+	return new MessageActionRow<MessageButton>().addComponents(createMessageDeleteButton(userId));
+}
+
+/**
+ * Creates an array of MessageActionRows that with one MessageActionRow containing one Delete button.
+ * The button responds only the given user and deletes the message the button is attached to.
+ */
+export function createMessageDeleteButtonComponents(userId: Snowflake): MessageActionRow<MessageActionRowComponent>[] {
+	return [createMessageDeleteButtonRow(userId)];
+}
+
+/** Adds a Delete button to the given message that responds only the given user and deletes the attached message. */
 export async function addMessageDeleteButton(message: Optional<DMessage>, userId: Snowflake): Promise<boolean> {
 	if (message?.editable) {
-		/** @todo update all Sage "delete" icons to use custom trashcan or wastebin emoji 🗑️ instead of ❌ */
-		const buttonRow = new MessageActionRow<MessageButton>().addComponents(new MessageButton({ customId:`message-delete-button-${message.id}-${userId}`, style:"SECONDARY", emoji:"❌", label:"Delete this alert.", type:"BUTTON" }));
-		const edited = await message.edit({
-			components: (message.components ?? []) .concat([buttonRow as any]), /** @todo figure out this cast */
-			content: message.content ? message.content : undefined,
-			embeds: message.embeds
-		}).catch(handleDiscordErrorReturnNull);
+		const components = (message.components ?? []).concat(createMessageDeleteButtonComponents(userId));
+		const content = message.content ? message.content : undefined;
+		const embeds = message.embeds;
+		const files = [...message.attachments.values()];
+		const edited = await message.edit({ components, content, embeds, files }).catch(handleDiscordErrorReturnNull);
 		return edited !== null;
 	}
 	return false;
 }
 
+/** Checks the interaction for the customId used for deleting messages. */
 function messageDeleteButtonTester(sageInteraction: SageInteraction<ButtonInteraction>): boolean {
 	if (sageInteraction.interaction.isButton()) {
 		const customId = sageInteraction.interaction.customId;
-		const regex = /^message-delete-button-(\d{16,})-(\d{16,})$/;
-		const match = regex.exec(customId) ?? [];
-		return sageInteraction.interaction.message.id === match[1]
-			&& sageInteraction.user.id === match[2];
+		const regex = /^rpg-sage-message-delete-button-(\d{16,})$/;
+		const [_, userId] = regex.exec(customId) ?? [];
+		if (userId) {
+			return sageInteraction.user.id === userId;
+		}
 	}
 	return false;
 }
 
+/** Handles the interaction used for deleting messages. */
 async function messageDeleteButtonHandler(sageInteraction: SageInteraction<ButtonInteraction>): Promise<void> {
 	const message = sageInteraction.interaction.message as DMessage;
 	if (message.deletable) {
