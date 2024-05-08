ab36af0923b483004e2c21ecef4a1622ad2767fe
Randal T Meyer <randal.t.meyer@gmail.com>
2024-05-08 16:18:40 -0700

    change to SageCommand

--- src/sage-lib/sage/commands/admin/GameCharacter/testCanAdminCharacter.ts
+++ src/sage-lib/sage/commands/admin/GameCharacter/testCanAdminCharacter.ts
@@ -1,15 +1,15 @@
-import type { SageMessage } from "../../../model/SageMessage";
-import type { TCharacterTypeMeta } from "./getCharacterTypeMeta";
+import type { SageCommand } from "../../../model/SageCommand.js";
+import type { TCharacterTypeMeta } from "./getCharacterTypeMeta.js";
 
-export function testCanAdminCharacter(sageMessage: SageMessage, characterTypeMeta: TCharacterTypeMeta): boolean {
-	if (!sageMessage.allowAdmin) {
+export function testCanAdminCharacter(sageCommand: SageCommand, characterTypeMeta: TCharacterTypeMeta): boolean {
+	if (!sageCommand.allowAdmin) {
 		return false;
 	}
 
-	if (sageMessage.game) {
+	if (sageCommand.game) {
 		return characterTypeMeta.isPcOrCompanion
-			? sageMessage.isGameMaster || sageMessage.isPlayer
-			: sageMessage.isGameMaster;
+			? sageCommand.isGameMaster || sageCommand.isPlayer
+			: sageCommand.isGameMaster;
 	}
 
 	return characterTypeMeta.isPcOrCompanion;
