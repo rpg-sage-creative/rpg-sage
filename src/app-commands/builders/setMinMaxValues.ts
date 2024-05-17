46bb22eff835b0ef0709ed4ace892f15ae553a29
Randal T Meyer <randal.t.meyer@gmail.com>
2024-05-16 20:15:17 -0700

    slash builder type updates

--- src/app-commands/builders/setMinMaxValues.ts
+++ src/app-commands/builders/setMinMaxValues.ts
@@ -1,8 +1,8 @@
-import type { SlashCommandNumberOption } from "@discordjs/builders";
+import type { SlashCommandIntegerOption, SlashCommandNumberOption } from "@discordjs/builders";
 import { isValidNumber } from "@rsc-utils/number-utils";
 import type { SlashCommandOption } from "../types.js";
 
-export function setMinMaxValues<T extends SlashCommandNumberOption>(opt: T, option: SlashCommandOption): T {
+export function setMinMaxValues<T extends SlashCommandIntegerOption | SlashCommandNumberOption>(opt: T, option: SlashCommandOption): T {
 	if (isValidNumber(option.minValue)) {
 		opt.setMinValue(option.minValue);
 	}
