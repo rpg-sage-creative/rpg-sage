6d1aa685ba944ae32353b17b04d168da47d4adaa
Randal <randal.t.meyer@gmail.com>
2024-06-25 21:10:49 -0700

    simple cleanup

--- src/sage-lib/index.ts
+++ src/sage-lib/index.ts
@@ -1,6 +1,5 @@
 import { setSageId } from "@rsc-sage/env";
-import { error } from "@rsc-utils/core-utils";
-import { getCodeName } from "@rsc-utils/core-utils";
+import { error, getCodeName } from "@rsc-utils/core-utils";
 import { registerAndLoad } from "../sage-pf2e";
 import { registerPrompts } from "./discord/index.js";
 import { registerCommandHandlers } from "./sage/commands/index.js";
@@ -10,7 +9,7 @@ import { BotRepo } from "./sage/repo/BotRepo.js";
 export async function activate(): Promise<void> {
 	const botCodeName = getCodeName();
 	const bot = await BotRepo.getByCodeName(botCodeName).catch(err => {
-		error(`BotRepo.coreByCodeName("${botCodeName}") failed!`, err);
+		error(`BotRepo.getByCodeName("${botCodeName}") failed!`, err);
 		return null;
 	});
 	if (!bot) {
