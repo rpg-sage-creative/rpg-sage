ea503003185c495aa552a3353573e005a0d5dc2e
Randal <randal.t.meyer@gmail.com>
2024-07-30 16:13:17 -0700

    update discord-utils

--- packages/utils/discord-utils/src/modal/ModalBuilder.ts
+++ packages/utils/discord-utils/src/modal/ModalBuilder.ts
@@ -1,19 +1,42 @@
+import type { Optional } from "@rsc-utils/core-utils";
 import { ModalBuilder as _ModalBuilder, ActionRowBuilder, TextInputBuilder, TextInputStyle } from "discord.js";
+
+type Options = {
+	maxLength?: number;
+	required?: boolean;
+};
 
 export class ModalBuilder extends _ModalBuilder {
 
-	public addShortText(required?: boolean): TextInputBuilder {
+	public addShortText(): TextInputBuilder;
+	public addShortText(required: Optional<boolean>): TextInputBuilder;
+	public addShortText(options: Optional<Options>): TextInputBuilder;
+	public addShortText(arg?: Optional<boolean | Options>): TextInputBuilder {
 		const index = this.components.length;
 		const row = new ActionRowBuilder<TextInputBuilder>();
 		const input = new TextInputBuilder();
-		input.setStyle(TextInputStyle.Short).setCustomId(`field-${index}`).setRequired(required);
+		input.setStyle(TextInputStyle.Short);
+		input.setCustomId(`field-${index}`);
+		if (arg) {
+			if (typeof(arg) === "boolean") {
+				input.setRequired(arg === true);
+			}else {
+				input.setRequired(arg?.required === true);
+				if (arg.maxLength) {
+					input.setMaxLength(arg.maxLength);
+				}
+			}
+		}
 		row.addComponents(input);
 		this.addComponents(row);
 		return input;
 	}
 
-	public addParagraph(required?: boolean): TextInputBuilder {
-		return this.addShortText(required).setStyle(TextInputStyle.Paragraph);
+	public addParagraph(): TextInputBuilder;
+	public addParagraph(required: Optional<boolean>): TextInputBuilder;
+	public addParagraph(options: Optional<Options>): TextInputBuilder;
+	public addParagraph(arg?: Optional<boolean | Options>): TextInputBuilder {
+		return this.addShortText(arg as Options).setStyle(TextInputStyle.Paragraph);
 	}
 
 }
