21280df808d9a1a91f6ca24f5987591ae354be6e
Randal <randal.t.meyer@gmail.com>
2024-07-29 20:26:33 -0700

    use replyStack.defer

--- src/sage-lib/sage/commands/default.ts
+++ src/sage-lib/sage/commands/default.ts
@@ -221,19 +221,18 @@ async function findHandler(sageMessage: SageMessage): Promise<void> {
 //#region dm slash command
 
 async function dmSlashHandler(sageInteraction: SageInteraction): Promise<void> {
-	return sageInteraction.defer(true).then(deferred, failure);
-
-	function deferred(): Promise<void> {
-		const dmContent = `Hello!\nRPG Sage will now reply to your Direct Messages.\n*Note: Anytime RPG Sage is disconnected from Discord, you may need to reestablish this connection. We apologize for the inconvenience.*`;
-		return sageInteraction.user.send(dmContent).then(success, failure);
-	}
-	function success(): Promise<void> {
-		return sageInteraction.whisper(`Please check your DMs!`);
-	}
-	function failure(reason: any): Promise<void> {
+	const failure = (reason: any) => {
 		error(reason);
 		return sageInteraction.whisper(`Sorry, there was a problem!`);
-	}
+	};
+	const deferred = () => {
+		const success = () => {
+			return sageInteraction.whisper(`Please check your DMs!`);
+		};
+		const dmContent = `Hello!\nRPG Sage will now reply to your Direct Messages.\n*Note: Anytime RPG Sage is disconnected from Discord, you may need to reestablish this connection. We apologize for the inconvenience.*`;
+		return sageInteraction.user.send(dmContent).then(success, failure);
+	};
+	return sageInteraction.replyStack.defer().then(deferred, failure);
 }
 
 //#endregion
