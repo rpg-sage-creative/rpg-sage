6bf84e16697c685669e125ce05c2aa14d636aa7d
Randal <randal.t.meyer@gmail.com>
2024-08-13 09:22:09 -0700

    improve findPc to allow multiple characters in a game

--- src/sage-lib/sage/commands/dialog/find/findPc.ts
+++ src/sage-lib/sage/commands/dialog/find/findPc.ts
@@ -4,14 +4,40 @@ import type { GameCharacter } from "../../../model/GameCharacter.js";
 import type { SageCommand } from "../../../model/SageCommand.js";
 
 export function findPc(sageCommand: SageCommand, pcNameOrIndex: Optional<string>): GameCharacter | undefined {
-	if (sageCommand.game) {
-		return sageCommand.playerCharacter;
-	}
-	if (sageCommand.allowDialog) {
-		if (isBlank(pcNameOrIndex)) {
-			return sageCommand.sageUser.playerCharacters.first();
+	if (!sageCommand.allowDialog) return undefined;
+
+	const { authorDid, game, sageUser } = sageCommand;
+	const gamePcs = game?.playerCharacters;
+	const userPcs = sageUser.playerCharacters;
+
+	let char: GameCharacter | undefined;
+
+	// try by given name/index first
+	if (!isBlank(pcNameOrIndex)) {
+		const index = +pcNameOrIndex;
+		if (isNaN(index)) {
+			char = gamePcs?.filterByUser(authorDid).findByName(pcNameOrIndex)
+				?? userPcs.findByName(pcNameOrIndex);
+		}else {
+			char = gamePcs?.filterByUser(authorDid)[index]
+				?? userPcs[index];
 		}
-		return sageCommand.sageUser.playerCharacters.findByNameOrIndex(pcNameOrIndex);
 	}
-	return undefined;
+
+	// try grabbing auto character
+	if (!char) {
+		const autoChannel = { channelDid:sageCommand.channelDid!, userDid:authorDid };
+		char = gamePcs?.getAutoCharacter(autoChannel)
+			?? userPcs.getAutoCharacter(autoChannel)
+			?? undefined;
+	}
+
+	// else grab their first
+	if (!char) {
+		char = gamePcs
+			? gamePcs.findByUser(authorDid)
+			: userPcs.first();
+	}
+
+	return char;
 }
