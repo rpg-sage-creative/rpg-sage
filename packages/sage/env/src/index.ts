03cc46bc29248fc93373b03d87baa1dd9ede2479
Randal <randal.t.meyer@gmail.com>
2024-10-03 06:33:57 -0700

    add hasTupperBoxId and isTupperBoxId

--- packages/sage/env/src/index.ts
+++ packages/sage/env/src/index.ts
@@ -2,6 +2,6 @@ export * from "./getHomeServerId.js";
 export * from "./getRollemId.js";
 export * from "./getSuperAdminId.js";
 export * from "./getSuperUserId.js";
-export * from "./getTupperBoxId.js";
 export * from "./SageId.js";
+export * from "./TupperBoxId.js";
 export * from "./WebhookName.js";
