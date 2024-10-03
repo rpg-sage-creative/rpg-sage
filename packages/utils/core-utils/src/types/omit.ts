d911fda2696a85b78fe9455d1792d4413cf933dd
Randal <randal.t.meyer@gmail.com>
2024-10-03 13:39:55 -0700

    update core-utils

--- packages/utils/core-utils/src/types/omit.ts
+++ packages/utils/core-utils/src/types/omit.ts
@@ -1,5 +1,19 @@
-/** Returns a new object by omitting the given keys from the given object. This is a shallow copy, meaning any values that are objects will be references to the original. */
-export function omit<T, U extends keyof T, V extends Omit<T, U>>(object: T, ...omittedKeys: U[]): V {
+import type { Optional, OrNull, OrUndefined } from "./generics.js";
+import { isNullOrUndefined } from "./typeGuards/isNullOrUndefined.js";
+
+/**
+ * Returns a new object by omitting the given keys from the given object.
+ * This is a shallow copy, meaning any values that are objects will be references to the original.
+ * If the object given is null, null is returned.
+ * If the object given is undefined, undefined is returned.
+ */
+export function omit<T, U extends keyof T, V extends Omit<T, U>>(object: T, ...omittedKeys: U[]): V;
+export function omit<T, U extends keyof T, V extends Omit<T, U>>(object: OrNull<T>, ...omittedKeys: U[]): OrNull<V>;
+export function omit<T, U extends keyof T, V extends Omit<T, U>>(object: OrUndefined<T>, ...omittedKeys: U[]): OrUndefined<V>;
+export function omit<T, U extends keyof T, V extends Omit<T, U>>(object: Optional<T>, ...omittedKeys: U[]): Optional<V>;
+export function omit<T, U extends keyof T, V extends Omit<T, U>>(object: Optional<T>, ...omittedKeys: U[]): Optional<V> {
+	if (isNullOrUndefined(object)) return object;
+
 	const out: V = {} as any;
 	const keys = Object.keys(object as object) as U[];
 	keys.forEach(key => {
