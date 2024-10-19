10e4de58dab50eb0ede725350edcb9710ded356c
Randal <randal.t.meyer@gmail.com>
2024-10-18 22:15:58 -0700

    fix doStatMath to solve complex math and clean multiple pos/neg modifiers

--- packages/utils/dice-utils/src/stats/doStatMath.ts
+++ packages/utils/dice-utils/src/stats/doStatMath.ts
@@ -1,6 +1,7 @@
 import { tokenize } from "../internal/tokenize.js";
+import { getComplexRegex } from "../math/doComplex.js";
+import { doPosNeg, getPosNegRegex } from "../math/doPosNeg.js";
 import { processMath } from "../math/processMath.js";
-import { getDiceRegex } from "../token/getDiceRegex.js";
 
 /**
  * Checks the stat value for math.
@@ -10,8 +11,19 @@ import { getDiceRegex } from "../token/getDiceRegex.js";
  * (Primarily for hiding values, such as AC.)
  */
 export function doStatMath(value: string): string {
+	const options = { allowSpoilers:true };
+	const parsers = {
+		complex: getComplexRegex(options),
+		posNeg: getPosNegRegex(options),
+	};
+	const tokens = tokenize(value, parsers);
 	// process other math functions on non-dice parts of the value
-	const tokens = tokenize(value, { dice:getDiceRegex() });
-	const processed = tokens.map(({ token, key }) => key === "dice" ? token : processMath(token, { allowSpoilers:true }));
+	const processed = tokens.map(({ token, key }) => {
+		switch(key) {
+			case "complex": return processMath(token, options);
+			case "posNeg": return doPosNeg(token, options);
+			default: return token;
+		}
+	});
 	return processed.join("");
 }
