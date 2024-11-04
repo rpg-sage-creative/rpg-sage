64c3d25bd2b378deba2822aa09b9dcb1555b9ec6
Randal <randal.t.meyer@gmail.com>
2024-11-04 09:27:55 -0800

    create logic for detecting and returing valid pathbuilder character json

---
+++ src/gameSystems/p20/import/pathbuilder-2e/validCoreOrUndefined.ts
@@ -0,0 +1,48 @@
+import type { TPathbuilderCharacter } from "../../../../sage-pf2e/model/pc/PathbuilderCharacter";
+
+type TPathbuilderJson = {
+	success: boolean;
+	build: TPathbuilderCharacter;
+};
+
+function isValidCore(json: unknown): json is TPathbuilderCharacter;
+function isValidCore(json: any): boolean {
+	if (json) {
+		return [
+			"name",
+			"class",
+			"level",
+			"ancestry",
+			"heritage",
+			"background",
+			"size",
+
+			"attributes", // ?
+
+			"abilities",
+			"proficiencies",
+
+			"weapons",
+			"money",
+			"armor",
+			"acTotal", // hephaistos is missing this
+		].every(key => key in json);
+	}
+	return false;
+}
+
+function isValidJson(json: unknown): json is TPathbuilderJson;
+function isValidJson(json: any): boolean {
+	if (json) {
+		return "success" in json
+			&& "build" in json
+			&& isValidCore(json.build);
+	}
+	return false;
+}
+
+export function validCoreOrUndefined(json: unknown): TPathbuilderCharacter | undefined {
+	if (isValidCore(json)) return json;
+	if (isValidJson(json)) return json.build;
+	return undefined;
+}
