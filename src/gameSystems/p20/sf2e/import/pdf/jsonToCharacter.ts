4585cdea29069664c5fddcde319d47896805f994
Randal <randal.t.meyer@gmail.com>
2024-11-04 09:28:52 -0800

    split logic to return core or character for reusability

--- src/gameSystems/p20/sf2e/import/pdf/jsonToCharacter.ts
+++ src/gameSystems/p20/sf2e/import/pdf/jsonToCharacter.ts
@@ -1,12 +1,12 @@
 import { PdfJsonFieldManager, type PdfJson } from "@rsc-utils/io-utils";
-import { PathbuilderCharacter } from "../../../../../sage-pf2e/model/pc/PathbuilderCharacter.js";
+import { PathbuilderCharacter, type TPathbuilderCharacter } from "../../../../../sage-pf2e/model/pc/PathbuilderCharacter.js";
 import { getDemiplanePdfKeyMap } from "./keyMap/getDemiplanePdfKeyMap.js";
 import { getPlaytestPdfKeyMap } from "./keyMap/getPlaytestPdfKeyMap.js";
 import { parseCharacterCore } from "./parseCharacterCore.js";
 
 type Field = { name:string; value:string; } | { name:string; checked:boolean; };
 
-export function jsonToCharacter(rawJson: PdfJson): PathbuilderCharacter | undefined {
+export function jsonToCharacterCore(rawJson: PdfJson): TPathbuilderCharacter | undefined {
 	const isDemiplane = rawJson.Pages.some(page => page.Fields.some((field: { id?:{ Id?:string; } }) => field.id?.Id === "character_name"));
 	const pdfKeyMap = isDemiplane
 		? getDemiplanePdfKeyMap()
@@ -20,6 +20,11 @@ export function jsonToCharacter(rawJson: PdfJson): PathbuilderCharacter | undefi
 	if (!mgr.has("name")) return undefined;
 
 	const core = parseCharacterCore(mgr, pdfKeyMap);
+	return core ?? undefined;
+}
+
+export function jsonToCharacter(rawJson: PdfJson): PathbuilderCharacter | undefined {
+	const core = jsonToCharacterCore(rawJson);
 	if (core) return new PathbuilderCharacter(core);
 	return undefined;
 }
