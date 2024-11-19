ca0acd097f25dafed596ca4576514771e107237a
Randal <randal.t.meyer@gmail.com>
2024-11-18 16:45:59 -0800

    allow reading DialogMessage data by MessageReference

--- src/sage-lib/sage/repo/DialogMessageRepository.ts
+++ src/sage-lib/sage/repo/DialogMessageRepository.ts
@@ -1,16 +1,20 @@
-import { errorReturnFalse, errorReturnNull } from "@rsc-utils/core-utils";
-import type { DiscordKey } from "@rsc-utils/discord-utils";
+import { errorReturnFalse, errorReturnNull, type Snowflake } from "@rsc-utils/core-utils";
+import { DiscordKey } from "@rsc-utils/discord-utils";
 import { readJsonFile, writeFile } from "@rsc-utils/io-utils";
+import type { MessageReference } from "discord.js";
 import type { TDialogMessage } from "../model/GameCharacter";
 import { IdRepository } from "./base/IdRepository";
 
-function toPath(discordKey: DiscordKey): string {
-	return `${IdRepository.DataPath}/messages/${discordKey.shortKey}.json`;
+function toPath(keyOrRef: DiscordKey | MessageReference): string {
+	const fileName = keyOrRef instanceof DiscordKey
+		? keyOrRef.shortKey
+		: DiscordKey.createKey(keyOrRef.guildId as Snowflake, keyOrRef.messageId as Snowflake);
+	return `${IdRepository.DataPath}/messages/${fileName}.json`;
 }
 
 export class DialogMessageRepository {
-	public static async read(discordKey: DiscordKey, catcher = errorReturnNull): Promise<TDialogMessage | null> {
-		const path = toPath(discordKey);
+	public static async read(keyOrRef: DiscordKey | MessageReference, catcher = errorReturnNull): Promise<TDialogMessage | null> {
+		const path = toPath(keyOrRef);
 		return readJsonFile<TDialogMessage>(path).catch(catcher);
 	}
 	public static async write(discordKey: DiscordKey, dialogMessage: TDialogMessage): Promise<boolean> {
