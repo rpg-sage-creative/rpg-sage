26c01c73659f23bcc1437fbb6564a5eef984f43f
Randal <randal.t.meyer@gmail.com>
2024-12-19 17:01:04 -0800

    add more texts

--- src/sage-lang/getLocalizedText.ts
+++ src/sage-lang/getLocalizedText.ts
@@ -2,6 +2,12 @@ import { stringFormat } from "@rsc-utils/string-utils";
 
 const enUS = {
 	"DISCORD_POLICY": `(Discord Policy)`,
+
+	"CANNOT_CREATE_CHARACTERS_HERE": `Sorry, you cannot create characters here.`,
+	"CANNOT_IMPORT_CHARACTERS_HERE": `Sorry, you cannot import characters here.`,
+
+	"NPC_ONLY_IN_GAME": `Sorry, NPCs only exist inside a Game.`,
+
 	"USERNAME_BANNED": `The username is invalid due to your use of "#{0}". (Discord Policy)`,
 	"USERNAME_MISSING": `The username is missing.`,
 	"USERNAME_TOO_LONG": `The username is too long. (Discord Policy)`,
