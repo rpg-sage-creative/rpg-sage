d218b7f7a566c5d58b6045929b158a9a1f11e8f7
Randal <randal.t.meyer@gmail.com>
2025-01-08 12:11:38 -0800

    more enUS keys; improve types

--- src/sage-lang/getLocalizedText.ts
+++ src/sage-lang/getLocalizedText.ts
@@ -1,20 +1,43 @@
+import type { Optional } from "@rsc-utils/core-utils";
 import { stringFormat } from "@rsc-utils/string-utils";
 
 const DISCORD_POLICY = `[(Discord Policy)](<https://discord.com/developers/docs/resources/user>)`;
 
 const enUS = {
-	"CREATE": "Create",
-	"UPDATE": "Update",
-	"IMPORT": "Import",
-	"REIMPORT": "Reimport",
+
+	"CATEGORY": `Category`,
+	"CLOSE": `Close`,
+	"COMPANION": "Companion",
+	"CONTENT": `Content`,
+	"COPY": `Copy`,
+	"CREATE": `Create`,
+	"DELETE": `Delete`,
+	"DICE": `Dice`,
+	"EDIT": `Edit`,
+	"EMBED": `Embed`,
+	"FROM": `From`,
+	"IMPORT": `Import`,
+	"ITEMS": `Items`,
+	"MATH": `Math`,
+	"MINION": "Minion",
+	"NAME": `Name`,
+	"NEW": `New`,
+	"NOTE": `Note`,
+	"NPC": "NPC",
+	"PC": "PC",
+	"REIMPORT": `Reimport`,
+	"SHARE": `Share`,
+	"TABLE": `Table`,
+	"TO": `To`,
+	"UNCATEGORIZED": `Uncategorized`,
+	"UPDATE": `Update`,
+	"USAGE": `Usage`,
+	"WARNING": `Warning`,
+
+	"TABLE_URL": `Table Url`,
 
 	"IMPORT_ERROR": "Import Error!",
 	"REIMPORT_ERROR": "Reimport Error!",
-
-	"PC": "PC",
-	"NPC": "NPC",
-	"COMPANION": "Companion",
-	"MINION": "Minion",
 
 	"SORRY_WE_DONT_KNOW": "Sorry, we don't know what went wrong!",
 	"AT_LEAST_ONE_OCCURRED": `Sorry, at least one of the following occurred:`,
@@ -62,6 +85,46 @@ const enUS = {
 	"INVALID_PDF_URL": "The given PDF url is invalid.",
 	"INVALID_PDF": "The given PDF is invalid.",
 	"UNSUPPORTED_PDF": "The given PDF is unsupported.",
+
+	// MACROS
+	"CANNOT_DELETE_MACRO_HERE": `Sorry, you cannot delete macros here.`,
+	"CANNOT_MANAGE_MACRO_HERE": `Sorry, you cannot manage macros here.`,
+	"MACRO_DETAILS": `Macro Details`,
+	"MACRO_LIST": `Macro List`,
+	"NO_MACROS_FOUND": `No Macros Found!`,
+	"TO_VIEW_MACRO_USE_:": `To view a macro, use:`,
+
+	"INVALID_TABLE_DATA": `Invalid Table Data.`,
+
+	"CREATE_USER_MACRO": `Create User Macro`,
+	"EDIT_USER_MACRO": `Edit User Macro`,
+
+	"MACRO_NAME_PLACEHOLDER": `Macro names can only be letters and numbers.`,
+	"MACRO_CATEGORY_PLACEHOLDER": `Macro categories group related macros together.`,
+	"MACRO_TABLE_PLACEHOLDER": `The table you wish to roll on or the URL to the table.`,
+	"MACRO_ITEMS_PLACEHOLDER": `The items you wish to randomly select from.`,
+	"MACRO_DICE_PLACEHOLDER": `The dice you wish to roll, ex: [1d20 attack; 1d6 damage]`,
+
+	"CREATE_MACRO_?": `Create Macro?`,
+
+	"DELETE_MACRO_?": `Delete Macro?`,
+	"MACRO_S_DELETED": `Macro "#{0}" deleted.`,
+	"MACRO_S_NOT_DELETED": `Macro "#{0}" ***NOT*** deleted!`,
+
+	"UPDATE_MACRO_?": `Update Macro?`,
+	"MACRO_S_UPDATED": `Macro "#{0}" updated.`,
+	"MACRO_S_NOT_UPDATED": `Macro "#{0}" ***NOT*** updated!`,
+
+	"OVERRIDES_SAGE_DIALOG_EMOJI": `This overrides Sage dialog emoji:`,
+
+	// deleteButton
+	"DELETE_THIS_ALERT": `Delete this alert.`,
+	"MESSAGE_WAS_DELETED": `A message from RPG Sage to you was deleted in #{0} by #{1}.`,
+	"UNABLE_TO_DELETE_MESSAGE_:": `Sorry, RPG Sage is not able to delete this message: #{0}`,
+
+	"SAGE_MISSING_PERM_IN_THAT_CHANNEL": `RPG Sage doesn't appear to have the #{0} permission in that channel.`,
+	"SAGE_MISSING_ACCESS_TO_THAT_CHANNEL": `RPG Sage doesn't appear to have access to that channel.`,
+	"WE_TRIED_TO_DM_YOU": `We tried to DM this alert to you, but were unable to.`,
 };
 
 export type LocalizedTextKey = keyof typeof enUS;
@@ -72,7 +135,10 @@ const all = {
 
 export type Lang = keyof typeof all;
 
-type Arg = string | number;
+type Arg = Optional<string | number>;
+
+export type Localizer = (key: LocalizedTextKey, ...args: Arg[]) => string;
+
 export function getLocalizedText(key: LocalizedTextKey, lang: Lang, ...args: Arg[]): string {
 	return stringFormat(all[lang]?.[key] ?? enUS[key], ...args);
 }
