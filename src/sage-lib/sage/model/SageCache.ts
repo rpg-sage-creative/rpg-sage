bbdb04c0be53baf155bd9d6832dcd79cec00647b
Randal <randal.t.meyer@gmail.com>
2025-01-08 12:07:15 -0800

    correctly type getLocalizer()

--- src/sage-lib/sage/model/SageCache.ts
+++ src/sage-lib/sage/model/SageCache.ts
@@ -4,7 +4,7 @@ import { debug, errorReturnFalse, orNilSnowflake, parseUuid, silly, type Optiona
 import { canSendMessageTo, DiscordKey, fetchIfPartial, type DInteraction, type MessageChannel, type MessageOrPartial, type MessageTarget, type ReactionOrPartial, type UserOrPartial } from "@rsc-utils/discord-utils";
 import { toMarkdown } from "@rsc-utils/string-utils";
 import type { Channel, Client, User as DUser, Guild, GuildMember, Interaction, Message, MessageReference } from "discord.js";
-import { getLocalizedText, type LocalizedTextKey } from "../../../sage-lang/getLocalizedText.js";
+import { getLocalizedText, type Localizer } from "../../../sage-lang/getLocalizedText.js";
 import { DiscordCache } from "../../discord/DiscordCache.js";
 import { isDeleted } from "../../discord/deletedMessages.js";
 import { getPermsFor } from "../../discord/permissions/getPermsFor.js";
@@ -308,9 +308,9 @@ export class SageCache {
 		return this.server?.getPrefixOrDefault() ?? "";
 	}
 
-	public getLocalizer() {
+	public getLocalizer(): Localizer {
 		const lang = this.user.preferredLang ?? "en-US";
-		return (key: LocalizedTextKey, ...args: (string | number)[]) => getLocalizedText(key, lang, ...args);
+		return (key: any, ...args: any[]) => getLocalizedText(key, lang, ...args);
 	}
 
 	public async fetchChannel<T extends Channel = Channel>(channelId: Optional<Snowflake>): Promise<T | undefined> {
