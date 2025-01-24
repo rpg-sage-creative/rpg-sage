284792d5da32f03a8b67089d8c4514bf1dba3237
Randal <randal.t.meyer@gmail.com>
2025-01-23 16:36:40 -0800

    implement getWikiUrl

---
+++ src/sage-lang/getWikiUrl.ts
@@ -0,0 +1,16 @@
+const urls = {
+	"COLOR_MANAGEMENT": `https://github.com/rpg-sage-creative/rpg-sage/wiki/Color-Management`,
+	"EMOJI_MANAGEMENT": `https://github.com/rpg-sage-creative/rpg-sage/wiki/Emoji-Management`,
+	"IMPORT_CHARACTERS": `https://github.com/rpg-sage-creative/rpg-sage/wiki/Character-Management#importing-characters`,
+	"MACROS": `https://github.com/rpg-sage-creative/rpg-sage/wiki/Dice-Macros`,
+};
+
+export type WikiUrlKey = keyof typeof urls;
+
+export function getWikiUrl(key: WikiUrlKey): string {
+	return urls[key];
+}
+
+export function hasWikiUrl(key: string): key is WikiUrlKey {
+	return key in urls;
+}
