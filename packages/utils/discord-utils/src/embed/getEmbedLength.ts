dedc821f6a5a110c0c5f5fb3a07826ce9d676a88
Randal <randal.t.meyer@gmail.com>
2025-04-03 16:32:09 -0700

    smarter getEmbedLength

--- packages/utils/discord-utils/src/embed/getEmbedLength.ts
+++ packages/utils/discord-utils/src/embed/getEmbedLength.ts
@@ -1,7 +1,8 @@
+import type { Optional } from "@rsc-utils/core-utils";
 import { embedLength } from "discord.js";
 import { type EmbedResolvable } from "./EmbedResolvable.js";
 import { resolveEmbed } from "./resolveEmbed.js";
 
-export function getEmbedLength(embed: EmbedResolvable): number {
-	return embedLength(resolveEmbed(embed));
+export function getEmbedLength(embed: Optional<EmbedResolvable>): number {
+	return embed ? embedLength(resolveEmbed(embed)) : 0;
 }
