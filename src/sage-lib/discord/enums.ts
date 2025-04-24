a2007b1c6ac822ff15892596abc5d6eacf318275
Randal <randal.t.meyer@gmail.com>
2025-04-24 13:58:04 -0700

    remove unused enum

--- src/sage-lib/discord/enums.ts
+++ src/sage-lib/discord/enums.ts
@@ -1,5 +1,3 @@
-export enum SnowflakeType { ChannelReference, CustomEmoji, RoleMention, UserMention, Snowflake }
-
 export enum MessageType { Both = 0, Post = 1, Edit = 2 }
 
 export enum ReactionType { Both = 0, Add = 1, Remove = 2 }
