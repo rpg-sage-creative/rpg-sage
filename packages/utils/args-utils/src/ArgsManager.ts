59f76f6886194638c23a7f2f199d4e04f73e42e5
Randal <randal.t.meyer@gmail.com>
2025-05-29 21:44:03 -0700

    remove no longer needed args-utils

--- packages/utils/args-utils/src/ArgsManager.ts
+++
@@ -1,2 +0,0 @@
-import { ArgsManager as _ArgsManager } from "@rsc-utils/core-utils";
-export class ArgsManager<T extends string> extends _ArgsManager<T> { }
--- src/sage-lib/discord/ArgsManager.ts
+++ src/sage-lib/discord/ArgsManager.ts
@@ -1,4 +1,4 @@
-import { ArgsManager as _ArgsManager } from "@rsc-utils/args-utils";
+import { ArgsManager as _ArgsManager } from "@rsc-utils/core-utils";
 import { type OrUndefined, isNullOrUndefined } from "@rsc-utils/core-utils";
 import { type KeyValueArg, type TokenParsers, createKeyValueArgRegex, createQuotedRegex, createWhitespaceRegex, dequote, parseKeyValueArg, tokenize } from "@rsc-utils/string-utils";
 
