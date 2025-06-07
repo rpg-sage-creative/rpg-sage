222aa2385a6ce93cf315e53674056366af1c637e
Randal <randal.t.meyer@gmail.com>
2025-06-06 23:29:16 -0700

    update to isGuildBasedChannel

--- packages/utils/discord-utils/src/humanReadable/internal/channelToName.ts
+++ packages/utils/discord-utils/src/humanReadable/internal/channelToName.ts
@@ -1,15 +1,15 @@
 
 import { ZERO_WIDTH_SPACE, type Optional } from "@rsc-utils/core-utils";
 import type { Channel } from "discord.js";
-import { isDMBased, isGroupDMBased } from "../../types/types.js";
+import { isDMBasedChannel, isGroupDMBasedChannel } from "../../types/index.js";
 import { toGuildName } from "../toGuildName.js";
 import { toUserName } from "../toUserName.js";
 
 /** @internal */
 export function channelToName(channel: Optional<Channel>): string | undefined {
 	if (channel) {
-		if (isDMBased(channel)) {
-			if (isGroupDMBased(channel)) {
+		if (isDMBasedChannel(channel)) {
+			if (isGroupDMBasedChannel(channel)) {
 				return channel.recipients.map(toUserName).join(",");
 			}
 			return toUserName(channel.recipient);
