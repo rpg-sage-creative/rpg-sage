158b23970b9946bedf974964bbe661b60555d8a1
Randal <randal.t.meyer@gmail.com>
2025-06-11 14:48:56 -0700

    switch XRegExp to regex

--- packages/utils/dice-utils/src/token/getDiceRegex.ts
+++ packages/utils/dice-utils/src/token/getDiceRegex.ts
@@ -1,43 +1,40 @@
-import XRegExp from "xregexp";
+import { regex } from "regex";
 
 type Options = { globalFlag?: boolean; };
 
 export function getDiceRegex(options?: Options): RegExp {
-	const DICE_REGEX = XRegExp(`
-		([-+*/])?            # capture optional math sign
+	const flags = options?.globalFlag ? "gi" : "i";
+	return regex(flags)`
+		(?<sign> [\-+\/*] )?    # capture optional math sign
 
 		# optional non-capture group for fixed rolls
-		(?:
-			\\s*             # optional space
-			\\(              # open parentheses
+		(
+			\s*                 # optional space
+			\(                  # open parentheses
 				# non-optional capture group for fixed rolls
-				(
-					\\s*     # optional space
-					\\d*     # fixed roll value <-- (allow empty in the event of an empty macro arg)
-					(?:
-						\\s* # optional space
-						,    # comma delimiter for multiple values
-						\\s* # optional space
-						\\d+ # additional fixed roll values
+				(?<fixedValues>
+					\s*         # optional space
+					\d*         # fixed roll value <-- (allow empty in the event of an empty macro arg)
+					(
+						\s*     # optional space
+						,       # comma delimiter for multiple values
+						\s*     # optional space
+						\d+     # additional fixed roll values
 					)*
-					\\s*     # optional space
+					\s*         # optional space
 				)
-			\\)              # close parentheses
+			\)                  # close parentheses
 		)?
 
 		# non-optional non-capture group for die count / word break
-		(?:
-			\\s*             # optional space    <-- SHOULD WE ALLOW THIS?
-			(\\d+)           # capture die count
-			|                # or
-			\\b              # allows for "d20" (no count) while excluding "add20"
+		(
+			\s*                 # optional space    <-- SHOULD WE ALLOW THIS?
+			(?<dieCount> \d+ )  # capture die count
+			|                   # or
+			\b                  # allows for "d20" (no count) while excluding "add20"
 		)
 
-		d                    # die size indicator
-		(\\d+)               # capture die size
-	`, "xi");
-
-	return options?.globalFlag
-		? new RegExp(DICE_REGEX, "gi")
-		: DICE_REGEX;
+		d                       # die size indicator
+		(?<dieSize> \d+ )       # capture die size
+	`;
 }
