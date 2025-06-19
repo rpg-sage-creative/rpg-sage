c1bead91f0aaef4b669f5963b456898d24e99711
Randal <randal.t.meyer@gmail.com>
2025-06-19 16:09:23 -0700

    add deprectated comment

--- src/gameSystems/utils/numberOrUndefined.ts
+++ src/gameSystems/utils/numberOrUndefined.ts
@@ -1,5 +1,6 @@
 import { getNumberRegex, type Optional } from "@rsc-utils/core-utils";
 
+/** @deprecated update core-utils and use it from there */
 export function numberOrUndefined(value: Optional<string>): number | undefined {
 	if (value === null || value === undefined) return undefined;
 	if (getNumberRegex({ anchored:true }).test(value)) return +value;
