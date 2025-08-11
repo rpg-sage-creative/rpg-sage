4e474af0dd931f40c151a76d649bb56817df0eb2
Randal <randal.t.meyer@gmail.com>
2025-08-10 19:46:53 -0700

    updated keyvaluepair logic; temp until core-utils updated

--- packages/sage/types/src/generics.ts
+++ packages/sage/types/src/generics.ts
@@ -1,23 +1,35 @@
 export type KeyValuePair<Value extends number | string, Nil extends null | undefined> = {
+	/** the key as found */
 	key: string;
+	/** if the value is an empty string then Nil is returned */
 	value: Value | Nil;
 };
 
 export type KeyValueTrio<Value extends number | string, Nil extends null | undefined> = {
+	/** the key as found */
 	key: string;
+	/** used for comparisons: key.toLowerCase() */
 	keyLower: Lowercase<string>;
+	/** if the value is an empty string then Nil is returned */
 	value: Value | Nil;
 };
 
 export type KeyValueRegex<Value extends number | string, Nil extends null | undefined> = {
+	/** the key as found */
 	key: string;
+	/** regex for comparing the key: new RegExp(`^${key}$`, "i") */
 	keyRegex: RegExp;
+	/** if the value is an empty string then Nil is returned */
 	value: Value | Nil;
 };
 
 export type KeyValueQuad<Value extends number | string, Nil extends null | undefined> = {
+	/** the key as found */
 	key: string;
+	/** used for comparisons: key.toLowerCase() */
 	keyLower: Lowercase<string>;
+	/** regex for comparing the key: new RegExp(`^${key}$`, "i") */
 	keyRegex: RegExp;
+	/** if the value is an empty string then Nil is returned */
 	value: Value | Nil;
 };
