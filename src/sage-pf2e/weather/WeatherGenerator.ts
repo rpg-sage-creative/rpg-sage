7a9f97e4d260967d8eded3e2f723108869de5dfc
Randal <randal.t.meyer@gmail.com>
2025-08-20 16:51:09 -0700

    use SageDate logic in game-utils

--- src/sage-pf2e/weather/WeatherGenerator.ts
+++ src/sage-pf2e/weather/WeatherGenerator.ts
@@ -1,6 +1,5 @@
 import { Season, cloneJson, fahrenheitToCelsius, isDefined } from "@rsc-utils/core-utils";
-import { randomBoolean, randomInt, rollDiceString, rollDie, type SimpleDice } from "@rsc-utils/game-utils";
-import { GDate } from "../../sage-cal/pf2e/GDate.js";
+import { GDate, randomBoolean, randomInt, rollDiceString, rollDie, type SimpleDice } from "@rsc-utils/game-utils";
 import { WindStrength, rollOnTable, rollTemperatureVariation, type CloudCoverTableItem, type PrecipitationTableItem, type WindTableItem } from "../index.js";
 import {
 	ClimateType,
