c3facb60d22240bb2b389313ea111ac0f6d3ddd1
Randal <randal.t.meyer@gmail.com>
2025-08-26 10:47:25 -0700

    improve build.sh; add scrub.sh

--- scripts/sh/scrub.sh
+++ scripts/sh/scrub.sh
@@ -8,31 +8,23 @@ if [ ! -d "./.git" ]; then
 	echo "cd $(pwd)"
 fi
 
-# make sure we have something to build
-if [ ! -f "./tsconfig.json" ]; then
-	echo "Nothing to Build."
-	exit 1
-fi
-
 ROOT_DIR="$(pwd)"
 ROOT_NAME="$(basename -- "$(pwd)")"
-
-# we have a known issue with this lib needing this "declare module"
-if [ -d "./node_modules/pdf2json" ]; then
-	if [ ! -f "./node_modules/pdf2json/index.d.ts" ]; then
-		echo "Creating: ./node_modules/pdf2json/index.d.ts";
-		echo 'declare module "pdf2json";' > ./node_modules/pdf2json/index.d.ts
-	fi
-fi
 
 SCRIPT_DIR="$ROOT_DIR/scripts"
 INDEX_MJS="$SCRIPT_DIR/mjs/index.mjs"
 
-function buildModule() {
+function scrubModule() {
 	local repoName="$1"
 	local modulePath="$ROOT_DIR/modules/$repoName"
 
-	echo "Cleaning: $repoName ..."
+	echo "Scrubbing: $repoName ..."
+
+	cd "$modulePath"
+
+	# scrub node_modules folder
+	echo "  Deleting node_modules folder ..."
+	rm -rf node_modules
 
 	# scrub build folder
 	echo "  Deleting build folder ..."
@@ -42,14 +34,12 @@ function buildModule() {
 	echo "  Deleting tsbuildinfo files ..."
 	rm -f *.tsbuildinfo
 
-	echo "Building: $repoName ..."
+	# scrub package-lock
+	echo "  Deleting package-lock file ..."
+	rm -f package-lock.json
 
-	cd "$modulePath"
-
-	if [ ! -d "./node_modules" ]; then
-		echo "  Installing node_modules ..."
-		npm i
-	fi
+	echo "  Installing node_modules ..."
+	npm i
 
 	echo "  Generating index.ts files ..."
 	node "$INDEX_MJS" indexTs -r
@@ -64,14 +54,18 @@ function buildModule() {
 	if [ "$?" != "0" ]; then echo "Building: $repoName ... Failed!"; exit 1; fi
 }
 
-buildModule "core-utils"
-buildModule "io-utils"
-buildModule "discord-utils"
-buildModule "game-utils"
+scrubModule "core-utils"
+scrubModule "io-utils"
+scrubModule "discord-utils"
+scrubModule "game-utils"
 
 cd "$ROOT_DIR"
 
 echo "Cleaning: $ROOT_NAME ..."
+
+# scrub node_modules folder
+echo "  Deleting node_modules folder ..."
+rm -rf node_modules
 
 # scrub build folders
 echo "  Deleting build folders ..."
@@ -83,10 +77,8 @@ find . -type f -name '*.tsbuildinfo' -not -path './node_modules/*' -not -path '.
 
 echo "Building: $ROOT_NAME ..."
 
-if [ ! -d "./node_modules" ]; then
-	echo "  Installing node_modules ..."
-	npm i
-fi
+echo "  Installing node_modules ..."
+npm i
 
 # transpile the TS
 echo "  Transpiling ts files ..."
