8d277d920d7305299badb4ad1a50c14a7c758329
Randal <randal.t.meyer@gmail.com>
2025-08-30 23:06:44 -0700

    replace XRegExp with regex

--- packages/utils/dice-utils/src/removeDesc.ts
+++ packages/utils/dice-utils/src/removeDesc.ts
@@ -1,9 +1,9 @@
-import * as XRegExp from "xregexp";
-import { tokenize } from "./internal/tokenize.js";
+import { tokenize } from "@rsc-utils/core-utils";
+import { regex } from "regex";
 
 /** Removes the first instance of desc from description while ensuring it doesn't break HTML (ex: Removing "b" from "<b>8</b> b") */
 export function removeDesc(description: string, desc: string): string {
-	const tokens = tokenize(description, { html:/<[^>]+>/, desc:XRegExp(XRegExp.escape(desc)) });
+	const tokens = tokenize(description, { html:/<[^>]+>/, desc:regex`${desc}` });
 	const firstDesc = tokens.find(token => token.key === "desc");
 	return tokens
 		.filter(token => token !== firstDesc)
