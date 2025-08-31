09e442d6c604788418cd1e9ac6246cbae0e82b2d
Randal <randal.t.meyer@gmail.com>
2025-08-30 23:39:36 -0700

    add replaceTableMentions

--- src/sage-lib/sage/commands/dialog/chat/replaceCharacterMentions.ts
+++ src/sage-lib/sage/commands/dialog/chat/replaceCharacterMentions.ts
@@ -1,23 +1,22 @@
 import type { Snowflake } from "@rsc-utils/core-utils";
 import { toUserMention } from "@rsc-utils/discord-utils";
 import { dequote, getQuotedRegexSource } from "@rsc-utils/string-utils";
-import XRegExp from "xregexp";
 import { SageMessage } from "../../../model/SageMessage.js";
 
-export async function replaceCharacterMention(sageMessage: SageMessage, content: string): Promise<string> {
-	const charMentionRegex = XRegExp(`@\\w+|@${getQuotedRegexSource()}`, "g");
+export async function replaceCharacterMentions(sageMessage: SageMessage, content: string): Promise<string> {
+	const charMentionRegex = new RegExp(`@\\w+|@${getQuotedRegexSource()}`, "g");
 	if (!charMentionRegex.test(content)) {
 		return content;
 	}
 
-	const gmUser = await sageMessage.game?.gmGuildMember();
+	const { game, sageUser } = sageMessage;
+	const gmUser = await game?.gmGuildMember();
 	const gmUserId = gmUser?.id;
 
 	return content.replace(charMentionRegex, mention => {
 		const sliced = dequote(mention.slice(1));
 		let charName: string | undefined;
 		let userMention: string | undefined;
-		const { game } = sageMessage;
 		if (game) {
 			const charOrShell = game.findCharacterOrCompanion(sliced);
 			if (charOrShell) {
@@ -34,7 +33,7 @@ export async function replaceCharacterMention(sageMessage: SageMessage, content:
 				}
 			}
 		}else {
-			const char = sageMessage.sageUser.findCharacterOrCompanion(sliced);
+			const char = sageUser.findCharacterOrCompanion(sliced);
 			if (char) {
 				charName = char.name;
 				userMention = toUserMention(char.userDid) ?? "";
