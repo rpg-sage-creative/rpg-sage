bf4ec82af5cb774359db6071f49152a9d8e36469
Randal <randal.t.meyer@gmail.com>
2025-09-22 10:50:33 -0700

    improve typed regexp

--- packages/utils/core-utils/src/types/generics.ts
+++ packages/utils/core-utils/src/types/generics.ts
@@ -24,13 +24,51 @@ export type OrNull<T> = T | null;
 export type OrUndefined<T> = T | undefined;
 
 /** Applies the given Groups type to the groups value of the underlying RegExpExecArray. */
-export type TypedRegExpExecArray<Groups extends Record<Keys, Values>, Keys extends string = string, Values extends string = string> = RegExpExecArray & { groups:Groups; };
+export type TypedRegExpMatchArray<
+			Groups extends Record<Keys, Values>,
+			Keys extends string = string,
+			Values extends string | undefined = string | undefined
+		>
+	= RegExpMatchArray & { groups:Groups; };
+
+/** Applies the given Groups type to the groups value of the underlying RegExpExecArray. */
+export type TypedRegExpExecArray<
+			Groups extends Record<Keys, Values>,
+			Keys extends string = string,
+			Values extends string | undefined = string | undefined
+		>
+	= RegExpExecArray & { groups:Groups; };
 
 /** Applies the given Groups type to the groups value of the underlying RegExpExecArray values. */
-export type TypedRegExpStringIterator<Groups extends Record<Keys, Values>, Keys extends string = string, Values extends string = string> = RegExpStringIterator<TypedRegExpExecArray<Groups, Keys, Values>>;
+export type TypedRegExpStringIterator<
+			Groups extends Record<Keys, Values>,
+			Keys extends string = string,
+			Values extends string | undefined = string | undefined
+		>
+	= RegExpStringIterator<TypedRegExpExecArray<Groups, Keys, Values>>;
+
+export interface TypedRegExp<
+			Groups extends Record<Keys, Values>,
+			Keys extends string = string,
+			Values extends string | undefined = string | undefined
+		> extends RegExp {
+	exec(string: string): TypedRegExpExecArray<Groups> | null;
+}
 
 declare global {
 	interface String {
+		match<
+				Groups extends Record<Keys, Values>,
+				Keys extends string = string,
+				Values extends string | undefined = string | undefined
+			>(matcher: TypedRegExp<Groups>): TypedRegExpMatchArray<Groups>;
+
+		matchAll<
+				Groups extends Record<Keys, Values>,
+				Keys extends string = string,
+				Values extends string | undefined = string | undefined
+			>(regexp: TypedRegExp<Groups>): TypedRegExpStringIterator<Groups>;
+
 		toLowerCase<Case extends string = Lowercase<string>>(): Case;
 	}
 }
