bfabe98c5ec29df913087c84efdb4a2699ae33ee
Randal <randal.t.meyer@gmail.com>
2025-11-03 13:10:58 -0800

    reuse regexp

--- packages/utils/io-utils/src/https/getProtocol.ts
+++ packages/utils/io-utils/src/https/getProtocol.ts
@@ -1,6 +1,8 @@
 import followRedirects from "follow-redirects";
 
+const HttpUrlRegExp = /^http:\/\//i;
+
 /** Returns http if the url starts with http://, or https otherwise. */
 export function getProtocol(url: string): typeof followRedirects.http | typeof followRedirects.https {
-	return (/^http:\/\//i).test(url) ? followRedirects.http : followRedirects.https;
+	return HttpUrlRegExp.test(url) ? followRedirects.http : followRedirects.https;
 }
