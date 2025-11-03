911fc76cd06f7d60de937dec389a995b55f48caf
Randal <randal.t.meyer@gmail.com>
2025-11-03 13:43:29 -0800

    add direct import from hephaistos

--- src/gameSystems/sf1e/import/fetchCore.ts
+++ src/gameSystems/sf1e/import/fetchCore.ts
@@ -1,10 +1,13 @@
+import { parseJson } from "@rsc-utils/core-utils";
+import { getJson } from "@rsc-utils/io-utils";
 import type { SageCommand } from "../../../sage-lib/sage/model/SageCommand.js";
-import { fetchCores as fetchAll, fetchJsonCore, fetchCore as fetchOne, type FetchResult } from "../../utils/io/fetchCores.js";
+import { coreToResult, fetchCores as fetchAll, fetchCore as fetchOne, type FetchCoreResult, type FetchResult, type ImportHandlers } from "../../utils/io/fetchCores.js";
+import type { FetchResultError } from "../../utils/io/handleImportErrors.js";
 import { HephaistosCharacterSF1e } from "../characters/HephaistosCharacter.js";
 import { validCoreOrUndefined } from "./hephaistos/validCoreOrUndefined.js";
 import type { HephaistosCharacterCoreSF1e } from "./types.js";
 
-function createHandlers() {
+function createHandlers(): ImportHandlers<HephaistosCharacterCoreSF1e> {
 	return {
 		char: (core: HephaistosCharacterCoreSF1e) => new HephaistosCharacterSF1e(core),
 		pdf: () => undefined,
@@ -12,14 +15,36 @@ function createHandlers() {
 	};
 }
 
+type FetchedJson = { data: { characters:{ json:string; }[]; }; };
+
+async function fetchJsonCore(id: string, error: FetchResultError, handlers: ImportHandlers<HephaistosCharacterCoreSF1e>): Promise<FetchCoreResult<HephaistosCharacterCoreSF1e>> {
+	const postData = {"query":`{\n\tcharacters(readOnlyPermalinkId: "${id}") {\n\t\tjson\n\t}\n}`};
+	const fetchedJson = await getJson<FetchedJson>("https://hephaistos.online/query", postData).catch(() => undefined);
+	if (!fetchedJson) {
+		return { error };
+	}
+
+	const stringified = fetchedJson.data?.characters?.[0]?.json;
+	if (!stringified) {
+		return { error };
+	}
+
+	const json = parseJson(stringified);
+
+	const core = handlers.raw(json);
+	if (core) {
+		return coreToResult(core, handlers.char);
+	}
+
+	return { error:"UNSUPPORTED_JSON" };
+}
+
 /** Uses id="" from the import command as Pathbuilder's "Export JSON" id. */
 async function fetchById(sageCommand: SageCommand): Promise<FetchResult<HephaistosCharacterCoreSF1e> | undefined> {
-	const id = sageCommand.args.getNumber("id");
+	const id = sageCommand.args.getString("hephaistos-id") ?? sageCommand.args.getString("id");
 	if (id) {
-		fetchJsonCore;
-		// const exportUrl = `https://pathbuilder2e.com/json.php?id=${id}`;
-		// const coreOrError = await fetchJsonCore(exportUrl, "INVALID_ID", createHandlers());
-		// return { id, ...coreOrError };
+		const coreOrError = await fetchJsonCore(id, "INVALID_ID", createHandlers());
+		return { id, ...coreOrError };
 	}
 	return undefined;
 }
