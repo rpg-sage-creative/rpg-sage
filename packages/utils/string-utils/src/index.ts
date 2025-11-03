f9bd338a0e057756f34b70d6712e4d64efd72398
Randal <randal.t.meyer@gmail.com>
2025-11-03 13:37:10 -0800

    update core-utils markdown logic

--- packages/utils/string-utils/src/index.ts
+++ packages/utils/string-utils/src/index.ts
@@ -1,6 +1,5 @@
 export * from "./blank/index.js";
 export * from "./keyValueArg/index.js";
-export * from "./markdown/index.js";
 export * from "./normalize/index.js";
 export * from "./quote/index.js";
 export * from "./regex/index.js";
