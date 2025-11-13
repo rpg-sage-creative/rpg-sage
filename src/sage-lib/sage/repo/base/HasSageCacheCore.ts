144d222fcd513fca1fd1cc3fb37de05e3476cdaf
Randal <randal.t.meyer@gmail.com>
2025-11-13 14:09:20 -0800

    remove deprecate uuid getter

--- src/sage-lib/sage/repo/base/HasSageCacheCore.ts
+++ src/sage-lib/sage/repo/base/HasSageCacheCore.ts
@@ -1,4 +1,4 @@
-import { HasIdCore, type IdCore, type Snowflake, type UUID } from "@rsc-utils/core-utils";
+import { HasIdCore, type IdCore, type Snowflake } from "@rsc-utils/core-utils";
 import type { SageCache } from "../../model/SageCache.js";
 
 export class HasSageCacheCore<T extends IdCore<U>, U extends string = string> extends HasIdCore<T, U> {
@@ -6,7 +6,4 @@ export class HasSageCacheCore<T extends IdCore<U>, U extends string = string> ex
 
 	/** @deprecated transitional until all cores/entities no longer have did/uuid */
 	public get did(): Snowflake { return this.core.did ?? this.core.id as Snowflake; }
-
-	/** @deprecated transitional until all cores/entities no longer have did/uuid */
-	public get uuid(): UUID { return this.core.uuid ?? this.core.id as UUID; }
 }
