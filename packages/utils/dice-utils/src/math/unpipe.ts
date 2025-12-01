6e13ce56a8671433f47fb96126de8d192c451fdb
Randal <randal.t.meyer@gmail.com>
2025-12-01 15:10:34 -0800

    update unpipe to allow optional strings

--- packages/utils/dice-utils/src/math/unpipe.ts
+++ packages/utils/dice-utils/src/math/unpipe.ts
@@ -1,7 +1,25 @@
+import type { Optional } from "@rsc-utils/core-utils";
+
 const pipeRegex = /\|{2}[^|]+\|{2}/g;
 
+export type UnpipeResults<IsOptional extends boolean = false> = {
+	hasPipes: boolean;
+	unpiped: IsOptional extends true ? string | undefined : string;
+};
+
 /** Checks the value for piped ||spoilered|| text and returns the value without pipes along with the boolean for if pipes were found. */
-export function unpipe(value: string) {
+export function unpipe(value: string): UnpipeResults;
+
+/**
+ * Checks the value for piped ||spoilered|| text and returns the value without pipes along with the boolean for if pipes were found.
+ * If null or undefined are passed, hasPipes is false and unpiped is undefined.
+ */
+export function unpipe(value: Optional<string>): UnpipeResults<true>;
+
+export function unpipe(value: Optional<string>): UnpipeResults<any> {
+	if (value === null || value === undefined) {
+		return { hasPipes:false, unpiped:undefined };
+	}
 
 	// check for piped "hidden" values
 	const hasPipes = pipeRegex.test(value);
