6a5ceeabc1da5f6dc2cdbceb149652acdc78c8d8
Randal <randal.t.meyer@gmail.com>
2025-12-19 09:10:27 -0800

    improve toUserMention signature

--- packages/utils/discord-utils/src/mention/toUserMention.ts
+++ packages/utils/discord-utils/src/mention/toUserMention.ts
@@ -2,6 +2,8 @@ import type { Optional } from "@rsc-utils/core-utils";
 import { GuildMember, User, userMention } from "discord.js";
 import { resolveSnowflake, type SnowflakeResolvable } from "../resolve/resolveSnowflake.js";
 
+export function toUserMention(resolvable: SnowflakeResolvable | GuildMember | User): string;
+export function toUserMention(resolvable: Optional<SnowflakeResolvable | GuildMember | User>): string | undefined;
 export function toUserMention(resolvable: Optional<SnowflakeResolvable | GuildMember | User>): string | undefined {
 	const id = resolveSnowflake(resolvable);
 	return id ? userMention(id) : undefined;
