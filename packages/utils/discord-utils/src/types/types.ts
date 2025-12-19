72fd7cebe21f63cd08ca747c438b6f5bb8d55d6a
Randal <randal.t.meyer@gmail.com>
2025-12-19 09:14:09 -0800

    shim the old types to the new types

--- packages/utils/discord-utils/src/types/types.ts
+++ packages/utils/discord-utils/src/types/types.ts
@@ -1,38 +1,15 @@
-import type { Optional } from "@rsc-utils/core-utils";
-import type { MessageReference, AnySelectMenuInteraction, AnyThreadChannel, APIUser, AutocompleteInteraction, ButtonInteraction, CacheType, CategoryChannel, Channel, CommandInteraction, DiscordAPIError, DMChannel, ForumChannel, GuildBasedChannel, Interaction, MediaChannel, Message, MessageComponentInteraction, MessageReaction, ModalSubmitInteraction, NonThreadGuildBasedChannel, PartialDMChannel, PartialGroupDMChannel, PartialMessage, PartialMessageReaction, PartialRecipient, PartialUser, User, Partialize } from "discord.js";
-import type { DiscordApiError } from "../DiscordApiError.js";
-
-//#region types
+import type { APIUser, CacheType, Message, MessageReaction, MessageReference, Partialize, PartialMessage, PartialMessageReaction, PartialRecipient, PartialUser, User } from "discord.js";
+import type { SupportedInteraction, SupportedMessagesChannel, SupportedRepliableInteraction } from "./typeGuards/isSupported.js";
 
 export type MessageReferenceOrPartial = MessageReference | Omit<MessageReference, "type">;
 
-export type DInteraction<Cached extends CacheType = CacheType>
-	= ButtonInteraction<Cached> // button
-	| AnySelectMenuInteraction<Cached> // select
-	// there are more select menu interactions now
-	| MessageComponentInteraction<Cached> // button or select or text
-	| AutocompleteInteraction<Cached> // autocomplete
-	| CommandInteraction<Cached> // slash
-	| ModalSubmitInteraction<Cached> // modal
-	;
-
-export type DRepliableInteraction<Cached extends CacheType = CacheType>
-	= ButtonInteraction<Cached>
-	| AnySelectMenuInteraction<Cached>
-	// there are more select menu interactions now
-	| MessageComponentInteraction<Cached>
-	| CommandInteraction<Cached>
-	| ModalSubmitInteraction<Cached>
-	;
-
-type ChannelOrUser = Channel | UserOrPartial;
-
-export type DMBasedChannel = PartialGroupDMChannel | DMChannel | PartialDMChannel;
+export type DInteraction<Cached extends CacheType = CacheType> = SupportedInteraction<Cached>;
+export type DRepliableInteraction<Cached extends CacheType = CacheType> = SupportedRepliableInteraction<Cached>;
 
 /** Channel you can send a message to. */
-export type MessageChannel = Exclude<Channel, CategoryChannel | ForumChannel | MediaChannel | PartialGroupDMChannel>;
+export type MessageChannel = SupportedMessagesChannel;
 
-export type SMessage = Message & { channel:MessageChannel; };
+export type SMessage = Message & { channel:SupportedMessagesChannel; };
 export type SPartialMessage = Partialize<SMessage, 'type' | 'system' | 'pinned' | 'tts', 'content' | 'cleanContent' | 'author'>;
 
 export type SMessageOrPartial = SMessage | SPartialMessage;
@@ -41,7 +18,6 @@ export type MessageOrPartial = Message | PartialMessage;
 /** User or Channel you can send a message to. */
 export type MessageTarget = User | MessageChannel;
 
-export type NonThreadChannel = Exclude<Channel, AnyThreadChannel>;
 
 export type ReactionOrPartial = MessageReaction | PartialMessageReaction;
 
@@ -50,51 +26,8 @@ export type UserOrPartial = User | PartialUser;
 
 export type UserResolvable = User | PartialUser | APIUser | PartialRecipient;
 
-/** Channels that can have webhooks. */
-export type WebhookChannel = Exclude<NonThreadGuildBasedChannel, CategoryChannel | ForumChannel | MediaChannel>;
-
 //#endregion
 
-//#region type checks
 
-export function isChannel(value: Optional<ChannelOrUser>): value is Channel {
-	return value ? "isThread" in value : false;
-}
 
-export function isDMBased(value: Optional<ChannelOrUser>): value is DMBasedChannel {
-	return isChannel(value) && value.isDMBased();
-}
 
-export function isGroupDMBased(value: Optional<ChannelOrUser>): value is PartialGroupDMChannel {
-	return isDMBased(value) && "recipients" in value;
-}
-
-export function isGuildBased(value: Optional<ChannelOrUser>): value is GuildBasedChannel {
-	return isChannel(value) && "guild" in value;
-}
-
-export function isMessage<T extends MessageOrPartial>(value: Optional<Channel | Interaction | T | User | DiscordAPIError | DiscordApiError>): value is T {
-	return value ? "author" in value : false;
-}
-
-export function isMessageTarget(value: Optional<ChannelOrUser>): value is MessageTarget {
-	return value ? "send" in value : false;
-}
-
-export function isNonThread(value: Optional<ChannelOrUser>): value is NonThreadChannel {
-	return isChannel(value) && !value.isThread();
-}
-
-export function isThread(value: Optional<ChannelOrUser>): value is AnyThreadChannel {
-	return isChannel(value) && value.isThread();
-}
-
-export function isUser(value: Optional<ChannelOrUser>): value is UserOrPartial {
-	return value ? "createDM" in value : false;
-}
-
-export function isWebhookChannel(value: Optional<ChannelOrUser>): value is WebhookChannel {
-	return isChannel(value) && "fetchWebhooks" in value;
-}
-
-//#endregion
