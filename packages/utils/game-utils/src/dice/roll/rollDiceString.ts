5a545ab952190cdd7dad34f3b3d29ef79f2cca73
Randal <randal.t.meyer@gmail.com>
2026-01-17 17:49:06 -0800

    more efficient

--- packages/utils/game-utils/src/dice/roll/rollDiceString.ts
+++ packages/utils/game-utils/src/dice/roll/rollDiceString.ts
@@ -1,34 +1,49 @@
-import { sum } from "../sum.js";
+import { cleanWhitespace, isIntegerString, sum, type TypedRegExp } from "@rsc-utils/core-utils";
 import { rollDice } from "./rollDice.js";
+import { regex } from "regex";
+
+type SimpleDiceGroups = {
+	dSign?: "-"|"+";
+	dCount:`${number}`;
+	dSides:`${number}`;
+	mSign?:"-"|"+";
+	mValue?:`${number}`;
+};
+
+const SimpleDiceRegExp = regex("i")`
+	^
+	(?<dSign> [ \- \+ ] )?
+	(?<dCount> \d+ )
+	d
+	(?<dSides> \d+ )
+	(
+		(?<mSign> [ \- \+ ] )
+		(?<mValue> \d+ )
+	)?
+	$
+` as TypedRegExp<SimpleDiceGroups>;
 
 /**
  * If the diceString is an integer, that number is returned.
  * Otherwise, returns the results of rolling simple dice: 1d6 or 1d8+1 or 1d10-2.
- * Returns null if the input isn't a valid simple dice roll, or has 0 count or 0 sides.
+ * Returns undefined if the input isn't a valid simple dice roll.
  */
 export function rollDiceString(diceString: string): number | undefined {
-	const cleanDiceString = (diceString ?? "").replace(/\s+/g, "");
-	if (/^\d+$/.test(cleanDiceString)) {
+	const cleanDiceString = diceString ? cleanWhitespace(diceString, { replacement:"" }) : "";
+
+	if (isIntegerString(diceString)) {
 		return +cleanDiceString;
 	}
 
-	const regex = /^([-+])?(\d+)d(\d+)(?:([-+])(\d+))?$/i;
-	const match = regex.exec(cleanDiceString);
+	const match = SimpleDiceRegExp.exec(cleanDiceString);
 	if (!match) {
 		return undefined;
 	}
 
-	const diceSign = match[1];
-	const diceCount = +match[2];
-	const diceSides = +match[3];
-	if (!diceCount || !diceSides) {
-		return undefined;
-	}
+	const { dSign, dCount, dSides, mSign, mValue } = match.groups;
 
-	const modifierSign = match[4];
-	const modifier = +(match[5] ?? 0);
+	const val = sum(rollDice(+dCount, +dSides)) * (dSign === "-" ? -1 : 1);
+	const mod = +(mValue ?? 0) * (mSign === "-" ? -1 : 1);
 
-	const val = sum(rollDice(diceCount, diceSides)) * (diceSign === "-" ? -1 : 1);
-	const mod = modifier * (modifierSign === "-" ? -1 : 1);
 	return val + mod;
 }
