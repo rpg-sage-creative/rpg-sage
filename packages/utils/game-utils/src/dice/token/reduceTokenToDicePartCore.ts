91ce462700dfa53add98c071a3c4bece131648ea
Randal <randal.t.meyer@gmail.com>
2026-01-18 12:35:12 -0800

    improve types

--- packages/utils/game-utils/src/dice/token/reduceTokenToDicePartCore.ts
+++ packages/utils/game-utils/src/dice/token/reduceTokenToDicePartCore.ts
@@ -1,7 +1,7 @@
+import type { TokenData } from "@rsc-utils/core-utils";
 import { appendTestToCore } from "../DiceTest.js";
 import type { TDiceBaseCore } from "../dice/DiceBase.js";
 import type { DicePartCore } from "../dice/DicePart.js";
-import type { TokenData } from "../internal/tokenize.js";
 import { DiceDropKeep, type DiceDropKeepType } from "../manipulate/DiceDropKeep.js";
 import { appendManipulationToCore } from "../manipulate/appendManipulationToCore.js";
 import type { DiceOperator } from "../types/DiceOperator.js";
@@ -14,18 +14,21 @@ export type ReduceSignToDropKeep = {
 	test: (core: DicePartCore, token: TokenData) => boolean;
 };
 
+type DiceTokenData = TokenData & { key:"dice"; matches:[string, string, string, string]; };
 /**
  * Sets the core's .sign, .count, and .sides values from the tokens .matches
  * If reducePlusMinusToDropKeepData provided, a +2d20/-2d20 roll will be converted to 2d20kh1/2d20kl1 (Fortune/Misfortune; Advantage/Disadvantage)
  * */
-function reduceDiceToken<T extends DicePartCore>(core: T, token: TokenData, reduceSignToDropKeepData?: ReduceSignToDropKeep[]): boolean {
+function reduceDiceToken<T extends DicePartCore>(core: T, token: TokenData | DiceTokenData, reduceSignToDropKeepData?: ReduceSignToDropKeep[]): boolean {
 	if (token.key === "dice") {
 		let hasChanges = false;
 		if (token.matches) {
 			core.sign = token.matches[0] as DiceOperator;
+			/** @todo this should filter on valid numbers, not truthy numbers */
 			core.fixedRolls = (token.matches[1] ?? "").split(",").map(s => +s.trim()).filter(n => n);
 			core.count = +token.matches[2] || 0;
 			core.sides = +token.matches[3] || 0;
+			/** @todo this should test !token.matches[2]?.length instead of !core.count ... we should accept a 0, but not an empty string/undefined */
 			if (!core.count && core.sides) {
 				core.count = 1;
 			}
@@ -43,7 +46,8 @@ function reduceDiceToken<T extends DicePartCore>(core: T, token: TokenData, redu
 	return false;
 }
 
-function reduceModToken<T extends DicePartCore>(core: T, token: TokenData): T {
+type ModTokenData = TokenData<"mod"> & { matches:[string, string]; };
+function reduceModToken<T extends DicePartCore>(core: T, token: TokenData | ModTokenData): T {
 	if (token.key === "mod" && token.matches) {
 		core.sign = token.matches[0] as DiceOperator;
 		core.modifier = +token.matches[1] || 0;
