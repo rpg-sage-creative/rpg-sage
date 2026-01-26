881b4894c4ac6a7acde1c2d6b621d36cb5a5b734
Randal <randal.t.meyer@gmail.com>
2026-01-26 14:01:33 -0800

    simply deploy scripts

--- deploy/docker/post-deploy.sh
+++ deploy/docker/post-deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+echo "Reinstall ./node_modules"
+rm -rf node_modules
 npm ci
+
+echo "Rebuild app"
+rm -rf build
 npm run build
-pm2 start bot.config.cjs --env dev --only sage-bot
+
+echo "Restart process"
+pm2 startOrRestart bot.config.cjs --env dev --only sage-bot --updateEnv
--- deploy/docker/pre-deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-pm2 stop sage-bot-dev
-rm -rf node_modules
-rm -rf build
