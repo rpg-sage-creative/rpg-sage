fadab1acfd57f40f41add6299b2a02aa2d581f63
Randal <randal.t.meyer@gmail.com>
2026-01-26 22:24:12 -0800

    smarter deploy scripts

--- deploy-scripts/post-deploy.sh
+++ deploy-scripts/post-deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 codeName="$1"
+envCodeName="$codeName"
+if [ "$codeName" = "docker" ]; then
+	envCodeName="$dev"
+fi
 
 echo "Reinstall ./node_modules"
 rm -rf node_modules
@@ -13,5 +17,5 @@ npm run build
 echo "Copy ../config/env-$codeName.json to ./config/env.json"
 
 echo "Restart process"
-pm2 startOrRestart bot.config.cjs --env "$codeName" --only sage-bot --update-env
+pm2 startOrRestart bot.config.cjs --env "$envCodeName" --only sage-bot --update-env
 pm2 save --force
