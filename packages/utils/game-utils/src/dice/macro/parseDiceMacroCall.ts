11240a24076bf7b40a2c10532b06a0f858f6a245
Randal <randal.t.meyer@gmail.com>
2026-01-29 16:38:10 -0800

    fix macro logic to allow names with spaces

--- packages/utils/game-utils/src/dice/macro/parseDiceMacroCall.ts
+++ packages/utils/game-utils/src/dice/macro/parseDiceMacroCall.ts
@@ -16,11 +16,72 @@ export type DiceMacroParseResults = DiceMacroArgs & {
 	prefix?: DiceMacroPrefix;
 };
 
+/** Finds the macro with the longest name that matches the start of the given macroAndArgs. */
+function findBestTierMacro(macroTier: DiceMacroBase[], macroAndArgs: Lowercase<string>): DiceMacroBase | undefined {
+	let macro: DiceMacroBase | undefined;
+
+	// iterate all macros in the tier
+	for (const tierMacro of macroTier) {
+
+		// lowercase for comparison
+		const name = tierMacro.name.toLowerCase();
+
+		// see if the macro starts with the macro name
+		if (macroAndArgs.startsWith(name)) {
+
+			// now we need the macroAndArgs to be the same length as the macro name or we need a space after the macro name
+			if (name.length === macroAndArgs.length || macroAndArgs[name.length] === " ") {
+
+				// now we keep it if we don't have a macro yet or this name is longer
+				if (!macro || name.length > macro.name.length) {
+					macro = tierMacro;
+				}
+
+			}
+
+		}
+
+	}
+
+	return macro;
+}
+
+/**
+ * Finds the best macro of each tier and returns the one with the longest name.
+ * By iterating through tiers and only keeping later matches with longer names we ensure that lower tiers are prioritized in the case of duplicate names.
+ */
+function findBestMacro(macroTiers: DiceMacroBase[][], macroAndArgs: string): DiceMacroBase | undefined {
+	let macro: DiceMacroBase | undefined;
+
+	// lowercase for comparison
+	const macroAndArgsLower = macroAndArgs.toLowerCase();
+
+	// iterate all macros in the tier
+	for (const macroTier of macroTiers) {
+
+		// match best of this tier
+		const bestOfTier = findBestTierMacro(macroTier, macroAndArgsLower);
+
+		// only continue if we have a match
+		if (bestOfTier) {
+
+			// now we keep it if we don't have a macro yet or this name is longer
+			if (!macro || bestOfTier.name.length > macro.name.length) {
+				macro = bestOfTier;
+			}
+
+		}
+
+	}
+
+	return macro;
+}
+
 /**
  * Parses a dice macro call into prefix, macro, and args: [macro_name 0 ac="10"]
  */
 export function parseDiceMacroCall(diceMacroCall: string, macroTiers: DiceMacroBase[][]): DiceMacroParseResults | undefined {
-	const unwrappedMacroCall = unwrap(diceMacroCall, "[]");
+	const unwrappedMacroCall = unwrap(diceMacroCall, "[]").trim();
 
 	// first grab the prefix
 	const prefix = parseDiceMacroPrefix(unwrappedMacroCall);
@@ -28,20 +89,8 @@ export function parseDiceMacroCall(diceMacroCall: string, macroTiers: DiceMacroB
 	// next slice the macro and args from the prefix (trim in case we have a space)
 	const macroAndArgs = prefix ? unwrappedMacroCall.slice(prefix.prefixLength).trim() : unwrappedMacroCall;
 
-	// then ensure we have a valid macro name
-	const macroNameLower = macroAndArgs.split(" ")[0]!.toLowerCase();
-
-	// try finding the macro with the matching name in the lowest tier
-	let macro: DiceMacroBase | undefined;
-	for (const macroTier of macroTiers) {
-		for (const tierMacro of macroTier) {
-			if (tierMacro.name.toLowerCase() === macroNameLower) {
-				macro = tierMacro;
-				break; // breaks for (const tierMacro of macroTier) {
-			}
-		}
-		if (macro) break; // breaks for (const macroTier of macroTiers) {
-	}
+	// go find the best macro
+	const macro = findBestMacro(macroTiers, macroAndArgs);
 
 	// ensure we have a macro
 	if (!macro) return undefined;
