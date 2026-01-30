95cb9cd9795b3b9870c0714f66f3351a9bd32b36
Randal <randal.t.meyer@gmail.com>
2026-01-30 14:02:09 -0800

    strip leading zeros from numbers when doing eval to avoid octal literals

--- packages/utils/game-utils/src/dice/math/internal/evalMath.ts
+++ packages/utils/game-utils/src/dice/math/internal/evalMath.ts
@@ -2,10 +2,20 @@ import { error } from "@rsc-utils/core-utils";
 
 const SignPrefixRegExp = /^[+-]/;
 
+const LeadingZeroRegExp = /\b(?<!\d\.)0+([1-9])/g;
+
+/** Finds all numbers with leading zeros and removes the leading zeros. */
+function stripLeadingZeros(input: string): string {
+	return input.replaceAll(LeadingZeroRegExp, (_, num) => num);
+}
+
 export function evalMath(prepped: string): string {
 	try {
+		// Numbers with leading zeros get treated as octal, which is not allowed in strict mode.
+		const octalSafe = stripLeadingZeros(prepped);
+
 		// do the math
-		const outValue = eval(prepped);
+		const outValue = eval(octalSafe);
 
 		// it is possible to eval to undefined, treat as an error
 		if (outValue === null || outValue === undefined || isNaN(outValue)) {
