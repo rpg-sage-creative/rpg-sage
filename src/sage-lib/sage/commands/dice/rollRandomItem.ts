0fb49cb8e3c20f9a418784c035e02ceaf759f37d
Randal <randal.t.meyer@gmail.com>
2026-02-04 15:42:51 -0800

    add verbose flag to random item selection

--- src/sage-lib/sage/commands/dice/rollRandomItem.ts
+++ src/sage-lib/sage/commands/dice/rollRandomItem.ts
@@ -1,20 +1,60 @@
+import type { TypedRegExp } from "@rsc-utils/core-utils";
 import { randomItem } from "@rsc-utils/game-utils";
+import { regex } from "regex";
 import type { TDiceOutput } from "../../../../sage-dice/index.js";
 import type { SageCommand } from "../../model/SageCommand.js";
 
-const MatchRegExp = /^(?:(\d*)([ usgm]*)#)?(.*?)$/i;
-const UniqueRegExp = /u/i;
-const SortRegExp = /s/i;
-const SecretRegExp = /gm/i;
+type SecretPrefix = "gm";
+type SortPrefix = "s";
+type UniquePrefix = "u";
+type VerbosePrefix = "v";
+type Prefix = `${SecretPrefix|""}${SortPrefix|""}${UniquePrefix|""}${VerbosePrefix|""}`;
 
-/** Performs the random item selection and returns the results. */
+type Groups = {
+	number?: `${number}`;
+	prefix?: Prefix;
+	content: string;
+};
+
+const MatchRegExp = regex("i")`
+	^
+	(
+		(?<number> \d* )
+		(?<prefix>
+			(
+				\s  # allow spaces
+				|
+				gm  # gamemaster (aka secret)
+				|
+				s   # sort
+				|
+				u   # unique
+				|
+				v   # verbose
+			)*
+		)
+		\#
+	)?
+	(?<content> .*? )
+	$
+` as TypedRegExp<Groups>;
+
+/**
+ * Performs the random item selection and returns the results.
+ * Input should be unwrapped, as in no [].
+ */
 export function rollRandomItem(_: SageCommand, input: string): TDiceOutput[] {
-	const match = MatchRegExp.exec(input) ?? [];
-	const count = +match[1] || 1;
-	const unique = UniqueRegExp.test(match[2] ?? "");
-	const sort = SortRegExp.test(match[2] ?? "");
-	const hasSecret = SecretRegExp.test(match[2] ?? "");
-	const options = (match[3] ?? "").split(",").map(s => s.trim());
+	const match = MatchRegExp.exec(input);
+	if (!match) return [];
+
+	const { number = 1, prefix, prefixLower = prefix?.toLowerCase(), content } = match.groups;
+	const count = +number || 1;
+	const hasSecret = prefixLower?.includes("gm") === true;
+	const sort = prefixLower?.includes("s");
+	const unique = prefixLower?.includes("u");
+	const verbose = prefixLower?.includes("v");
+	const options = content.split(",").map(s => s.trim());
+
 	const selections: string[] = [];
 	const total = (unique ? Math.min(options.length, count) : count);
 	do {
@@ -23,13 +63,18 @@ export function rollRandomItem(_: SageCommand, input: string): TDiceOutput[] {
 			selections.push(random);
 		}
 	} while (selections.length < total && options.find(option => !selections.includes(option)));
+
 	if (sort) {
 		selections.sort();
 	}
+
+	const output = `${selections.join(", ")} \u27f5 ${input}`;
+	const inlineOutput = verbose ? output : selections.join(", ");
+
 	return [{
-		hasSecret: hasSecret,
-		inlineOutput: selections.join(", "),
+		hasSecret,
+		inlineOutput,
 		input: input,
-		output: `${selections.join(", ")} ${"\u27f5"} ${input}`
+		output,
 	}];
 }
