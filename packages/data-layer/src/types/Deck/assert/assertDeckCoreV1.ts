0816910a1c22451caa2297b46bee586cfb00c02d
Randal <randal.t.meyer@gmail.com>
2026-02-14 13:52:46 -0800

    extra deck validation

--- packages/data-layer/src/types/Deck/assert/assertDeckCoreV1.ts
+++ packages/data-layer/src/types/Deck/assert/assertDeckCoreV1.ts
@@ -1,6 +1,17 @@
-import { isNotBlank, isSnowflake } from "@rsc-utils/core-utils";
+import { isNotBlank, isSnowflake, sortPrimitive } from "@rsc-utils/core-utils";
 import { assertArray, assertNumber, assertSageCore, assertString, optional } from "../../../validation/index.js";
 import { DeckCoreV1Keys, type DeckCoreAny, type DeckCoreV1 } from "../index.js";
+
+export function assertEnglishDeck(core: DeckCoreV1, count: number): boolean {
+	const cards = ([] as number[])
+		.concat(core.discardPile ?? [])
+		.concat(core.drawPile ?? [])
+		.concat(core.hand ?? [])
+		.concat(core.spread ?? [])
+		;
+	cards.sort(sortPrimitive);
+	return cards.length === count && cards.every((value, index) => value === index);
+}
 
 const objectType = "Deck";
 export function assertDeckCoreV1(core: DeckCoreAny): core is DeckCoreV1 {
@@ -18,6 +29,8 @@ export function assertDeckCoreV1(core: DeckCoreAny): core is DeckCoreV1 {
 	if (!assertString({ core, objectType, key:"name", validator:isNotBlank, optional })) return false;
 	if (!assertArray({ core, objectType, key:"spread", validator:isValidCard, optional })) return false;
 	if (!assertString({ core, objectType, key:"type", validator:isValidDeck })) return false;
+	if (core.type === "English52" && !assertEnglishDeck(core, 52)) return false;
+	if (core.type === "English54" && !assertEnglishDeck(core, 54)) return false;
 
 	return true;
 }
