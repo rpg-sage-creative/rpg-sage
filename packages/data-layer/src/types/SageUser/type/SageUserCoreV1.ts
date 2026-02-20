fc57c4e36319b5106699a05c59d8031b1cf1c1b8
Randal <randal.t.meyer@gmail.com>
2026-02-19 22:53:58 -0800

    comment SageUserCoreV1

--- packages/data-layer/src/types/SageUser/type/SageUserCoreV1.ts
+++ packages/data-layer/src/types/SageUser/type/SageUserCoreV1.ts
@@ -2,27 +2,51 @@ import type { Snowflake, UUID } from "@rsc-utils/core-utils";
 import type { Alias, DialogDiceBehaviorType, DialogPostType, MacroBase, MoveDirectionOutputType, Note, SageCharacterCoreV1, SageCore } from "../../index.js";
 
 export type SageUserCoreV1 = SageCore<"User", Snowflake | UUID> & {
+
 	/** @deprecated use dialog templates */
 	aliases?: Alias[];
+
+	/** "on" (true) by default */
 	confirmationPrompts?: boolean;
+
 	defaultDialogType?: DialogPostType;
+
 	defaultSagePostType?: DialogPostType;
+
 	dialogDiceBehaviorType?: DialogDiceBehaviorType;
-	// did
+
+	// did (SageCore)
+
+	/** "off" (false) by default */
 	dmOnDelete?: boolean;
+
+	/** "on" (true) by default */
 	dmOnEdit?: boolean;
+
+	/** "-p" by default */
 	forceConfirmationFlag?: string;
-	// id
+
+	// id (SageCore)
+
 	macros?: MacroBase[];
+
 	mentionPrefix?: string;
+
 	moveDirectionOutputType?: MoveDirectionOutputType;
+
 	notes?: Note[];
-	// objectType
+
+	// objectType (SageCore)
+
 	/** @deprecated use single .characters array */
 	playerCharacters?: SageCharacterCoreV1[];
+
+	/** "-y" by default */
 	skipConfirmationFlag?: string;
-	// uuid
-	// ver
+
+	// uuid (SageCore)
+
+	// ver (SageCore)
 };
 
 export const SageUserV1Keys: (keyof SageUserCoreV1)[] = [
