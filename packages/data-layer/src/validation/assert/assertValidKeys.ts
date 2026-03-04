c4ff9e459b23a86a6739f099a6d1ac79b044da09
Randal <randal.t.meyer@gmail.com>
2026-03-03 20:54:44 -0800

    add option for requiredKeys

--- packages/data-layer/src/validation/assert/assertValidKeys.ts
+++ packages/data-layer/src/validation/assert/assertValidKeys.ts
@@ -1,17 +1,24 @@
 import { tagFailure } from "../index.js";
 
-type AssertValidKeysArgs<Core> = {
+type AssertValidKeysArgs<Core extends Record<string, any>> = {
 	core: Core;
 	objectType: string;
+	requiredKeys?: (keyof Core)[];
 	validKeys: (keyof Core)[];
 };
-export function assertValidKeys<Core>(args: AssertValidKeysArgs<Core>): boolean {
-	const { core, validKeys, objectType } = args;
+export function assertValidKeys<Core extends Record<string, any>>(args: AssertValidKeysArgs<Core>): boolean {
+	const { core, objectType, requiredKeys, validKeys } = args;
 
 	const coreKeys = Object.keys(core as object) as (keyof Core)[];
+
 	const invalidKeys = coreKeys.filter(key => !validKeys.includes(key));
 	if (invalidKeys.length) {
 		return tagFailure`${objectType}: invalid keys (${invalidKeys})`;
+	}
+
+	const missingKeys = requiredKeys?.filter(key => !(key in core));
+	if (missingKeys?.length) {
+		return tagFailure`${objectType}: missing keys (${missingKeys})`;
 	}
 
 	return true;
