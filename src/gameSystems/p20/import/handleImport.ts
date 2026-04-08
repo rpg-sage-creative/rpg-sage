095057aec3277925004a136f6338f587434e6c74
Randal <randal.t.meyer@gmail.com>
2026-04-08 11:11:07 -0700

    add information about pathbuilder reimport

--- src/gameSystems/p20/import/handleImport.ts
+++ src/gameSystems/p20/import/handleImport.ts
@@ -8,26 +8,26 @@ export async function handleImport(sageCommand: SageCommand): Promise<void> {
 	if (id) {
 		const content =
 `
-We are sorry, but importing your character directly from Pathbuilder 2e is disabled. Due to recent DDOS attacks against them, they were forced to change their site. Until we are able to work with them to support a safe way to import directly, we can only support manual imports.
+We are sorry, but importing your character directly from Pathbuilder 2e is disabled. Until we are able to support a safe way to import directly, we can only support manual imports.
 ### Steps to import manually:
-1. open your browser to <https://pathbuilder2e.com/json.php?id=${id}>
-2. copy all the json content on that page
+1. Open your browser to <https://pathbuilder2e.com/json.php?id=${id}>
+2. Copy all the json content on that page
   - macOS cmd + a then cmd + c
   - Win ctrl + a then ctrl + c
-3. create a simple plain text file
+3. Create a simple plain text file
   - any name will do, as long as it ends with \`.json\`
   - for simplicity, you can use \`${id}.json\`
-4. paste all the json into the file
+4. Paste all the json into the file
   - macOS cmd + v
   - Win ctrl + v
-5. start a new discord message
+5. Start a new discord message
   - drag/drop or attach the file to the message
   - for Pathfinder 2e, type: \`sage! import pf2e\`
   - for Starfinder 2e, type: \`sage! import sf2e\`
-6. send the message to trigger an import
+6. Send the message to trigger an import
 -# *for more help, visit us at <https://discord.gg/rpgsage>*
 `;
-    return sageCommand.replyStack.whisper({ content, ephemeral:true });
+		return sageCommand.replyStack.whisper({ content, ephemeral:true });
 	}
 	return _handleImport(sageCommand, { fetchCores, postCharacter });
 }
--- src/gameSystems/p20/import/handleReimport.ts
+++ src/gameSystems/p20/import/handleReimport.ts
@@ -6,6 +6,30 @@ import { handleReimport as _handleReimport } from "../../utils/io/handleReimport
 import { fetchCore } from "./fetchCore.js";
 
 export async function handleReimport(sageCommand: SageCommand, message: Message, characterId: string): Promise<void> {
+	const id = sageCommand.args.getNumber("id");
+	if (id) {
+		const content =
+`
+We are sorry, but reimporting your character directly from Pathbuilder 2e is disabled. Until we are able to support a safe way to reimport directly, we can only support manual reimports.
+### Steps to reimport manually:
+1. Open your browser to <https://pathbuilder2e.com/json.php?id=${id}>
+2. Copy all the json content on that page
+  - macOS cmd + a then cmd + c
+  - Win ctrl + a then ctrl + c
+3. Create a simple plain text file
+  - any name will do, as long as it ends with \`.json\`
+  - for simplicity, you can use \`${id}.json\`
+4. Paste all the json into the file
+  - macOS cmd + v
+  - Win ctrl + v
+5. Reply to the message with your imported character
+  - drag/drop or attach the file to your reply
+  - type: \`sage! reimport\`
+6. Send the reply to trigger a reimport
+-# *for more help, visit us at <https://discord.gg/rpgsage>*
+`;
+		return sageCommand.replyStack.whisper({ content, ephemeral:true });
+	}
 	const { loadCharacter } = PathbuilderCharacter;
 	await _handleReimport(sageCommand, message, characterId, { loadCharacter, fetchCore, updateSheet });
 }
