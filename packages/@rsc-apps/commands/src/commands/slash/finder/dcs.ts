ee8c787251d7120548ba1658cc4eeff7ff06be32
Randal <randal.t.meyer@gmail.com>
2026-09-19 13:25:51 -0700

    update -finder dcs slash to 2e terms

--- packages/@rsc-apps/commands/src/commands/slash/finder/dcs.ts
+++ packages/@rsc-apps/commands/src/commands/slash/finder/dcs.ts
@@ -1,15 +1,15 @@
 import type { GameSlashCommand } from "@rsc-utils/discord-utils";
 
-export function registerCommand(): GameSlashCommand<"PF2E"> {
+export function registerCommand(): GameSlashCommand<"Finder"> {
 	return {
-		game: "PF2E",
+		game: "Finder",
 		name: "DCs",
 		description: "Show Difficulty Classes",
 		options: [
 			{ name:"table", description:"Which DCs?", choices:[
 				{ name:"Simple", value:"simple" },
 				{ name:"By Level", value:"level" },
-				{ name:"By Spell Level", value:"spell" }
+				{ name:"By Spell Rank", value:"spell" }
 			], isRequired:true },
 			{ name:"proficiency", description:"What Proficiency?", choices:[
 				["Untrained", "U"],
